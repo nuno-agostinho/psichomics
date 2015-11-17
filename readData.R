07db2585b49ebe9aca0c0152bb45739c1ece2b08
nuno-agostinho <danielagostinho@sapo.pt>
2015-11-17 16:56:53 +0000

    Commands to get data from PSI files for MISO, MATS, VAST-TOOLS and SUPPA

---
+++ readData.R
@@ -0,0 +1,29 @@
+# MATS
+mats_JC.SE <- read.delim("/genedata/NunoA/psi_files/mats_JC.SE.txt")
+mats_JC.A3SS <- read.delim("/genedata/NunoA/psi_files/mats_JC.A3SS.txt")
+mats_JC.A5SS <- read.delim("/genedata/NunoA/psi_files/mats_JC.A5SS.txt")
+mats_JC.MXE <- read.delim("/genedata/NunoA/psi_files/mats_JC.MXE.txt")
+mats_JC.RI <- read.delim("/genedata/NunoA/psi_files/mats_JC.RI.txt")
+
+# VAST-TOOLS
+vast_all <- read.delim("/genedata/NunoA/psi_files/vast_all.tab")
+
+# MISO
+miso_A3SS <- read.delim("/genedata/NunoA/psi_files/miso_A3SS.miso_summary")
+miso_A5SS <- read.delim("/genedata/NunoA/psi_files/miso_A5SS.miso_summary")
+miso_SE <- read.delim("/genedata/NunoA/psi_files/miso_SE.miso_summary")
+miso_AFE <- read.delim("/genedata/NunoA/psi_files/miso_AFE.miso_summary")
+miso_ALE <- read.delim("/genedata/NunoA/psi_files/miso_ALE.miso_summary")
+miso_MXE <- read.delim("/genedata/NunoA/psi_files/miso_MXE.miso_summary")
+miso_RI <- read.delim("/genedata/NunoA/psi_files/miso_RI.miso_summary")
+miso_TandemUTR <- read.delim("/genedata/NunoA/psi_files/miso_TandemUTR.miso_summary")
+miso_isoforms <- read.delim("/genedata/NunoA/psi_files/miso_isoforms.miso_summary")
+
+# SUPPA
+suppa_A3.tab <- read.delim("/genedata/NunoA/psi_files/suppa_A3.tab.psi")
+suppa_A5.tab <- read.delim("/genedata/NunoA/psi_files/suppa_A5.tab.psi")
+suppa_AF.tab <- read.delim("/genedata/NunoA/psi_files/suppa_AF.tab.psi")
+suppa_AL.tab <- read.delim("/genedata/NunoA/psi_files/suppa_AL.tab.psi")
+suppa_MX.tab <- read.delim("/genedata/NunoA/psi_files/suppa_MX.tab.psi")
+suppa_RI.tab <- read.delim("/genedata/NunoA/psi_files/suppa_RI.tab.psi")
+suppa_SE.tab <- read.delim("/genedata/NunoA/psi_files/suppa_SE.tab.psi")
