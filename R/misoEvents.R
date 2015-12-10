670722a9d3198c08617e00101c7d75753b9b6175
nuno-agostinho <danielagostinho@sapo.pt>
2015-12-10 17:31:01 +0000

    Correction

--- R/misoEvents.R
+++ R/misoEvents.R
@@ -1,35 +1,67 @@
+#' Get rows of a data frame between two row indeces
+#'
+#' @details For a given iteration i, returns data from first_row[i] to
+#' last_row[i]
+#'
+#' @param i Integer: current iteration
+#' @param data Data.frame: contains the data of interest
+#' @param first_row Vector of integers: First row index of interest; value must
+#' be less than the respective last row index and less than the number of rows
+#' in the data frame
+#' @param last_row Vector of integers: Last row index of interest; value must be 
+#' higher than the respective first row index and less than the number of rows
+#' in the data frame
+#' 
+#' @return Data frame between two row indeces (returns NA if the first row index
+#' is NA)
+getRowsData <- function(i, data, first_row, last_row) {
+  first <- first_row[i]
+  last  <- last_row[i]
+  if (is.na(first)) {
+    # if there is no first position, there is no item match
+    return(NA)
+  } else if (is.na(last)) {
+    # if there's no last position, use the last row of the data frame
+    last <- nrow(data)
+  }
+  seq <- seq(first, last)
+  return(data[seq, 1:8])
+}
+
 #' Match MISO's splicing event IDs with the IDs present in the alternative
 #' splicing annotation file and get the event as a data frame
 #'
-#' @details More information about MISO available at 
-#' \url{http://miso.readthedocs.org}
+#' @details For faster execution times, provide a vector of event IDs.
+#' 
+#' For more information about MISO, see \url{http://miso.readthedocs.org}.
 #'
 #' @param eventID Character vector: alternative event IDs
 #' @param annotation Data.frame: alternative event annotation file
 #' @param IDcolumn Integer: index of the column with the event ID's in the
 #' alternative event annotation file
 #'
-#' @return Data frame of the matching event
+#' @return Data frame of the matching event (or NA when nothing is matched)
 #' @export
 #'
 #' @examples
-#' eventID <- "2217@uc002poi.1@uc002poe.1"
+#' eventID <- c("2217@uc002poi.1@uc002poe.1", "57705@uc009xob.1@uc001jgy.2")
 #' annotation <- read.delim("AFE.hg19.gff3", header=FALSE, comment.char="#")
 #' IDcolumn <- 9
-#' matchMisoEvent(eventID, annotation, IDcolumn)
+#' matchMisoEventID(eventID, annotation, IDcolumn)
 matchMisoEventID <- function(eventID, annotation, IDcolumn) {
-  # Get first line from annotation matching a given splicing event ID
-  event <- match(paste0("ID=", eventID,
+  # Get first row from annotation matching a given splicing event ID
+  index <- match(paste0("ID=", eventID,
                         ";Name=", eventID,
                         ";gid=", eventID),
                  annotation[[IDcolumn]])
-  # Get all lines related to a given splicing event
-  genes <- which(annotation[["V3"]] == "gene")
-  # Get index of the next gene
-  next_index <- genes[match(event, genes) + 1]
-  ret <- lapply(1:length(event), function(i)
-    return(annotation[index[i]:(next_index[i] - 1), ])
-  )
+  # Get every index of splicing events present in the annotation
+  events <- which(annotation[["V3"]] == "gene")
+  # Get the index of the next gene
+  next_index <- events[match(index, events) + 1]
+  
+  # Get the rows relative to each event
+  ret <- lapply(1:(length(index)), getRowsData, annotation, index,
+                next_index - 1)
   return(ret)
 }
 
@@ -39,21 +71,24 @@ matchMisoEventID <- function(eventID, annotation, IDcolumn) {
 #' \url{http://miso.readthedocs.org}
 #'
 #' @param event Data.frame containing only one event with at least 7 columns as 
-#' retrieved from the annotation files from MISO (GFF3 files)
+#' retrieved from the alternative splicing annotation files from MISO (GFF3
+#' files)
 #'
 #' @return List with event attributes and junction positions for the exons
 #' (depends on the events)
 #' @export
 #'
 #' @examples
-#' # alternative splicing event: retained introns (RI)
+#' # example of alternative splicing event: skipped exon (SE)
 #' event <- read.table(text = "
-#'   chr1 RI gene 17233 17742 . - .
-#'   chr1 RI mRNA 17233 17742 . - .
-#'   chr1 RI exon 17233 17742 . - .
-#'   chr1 RI mRNA 17233 17742 . - .
-#'   chr1 RI exon 17233 17364 . - .
-#'   chr1 RI exon 17601 17742 . - .")
+#'   chr1 SE gene 16854	18061	. - .
+#'   chr1 SE mRNA 16854 18061 . - .
+#'   chr1 SE exon 16854 17055 . - .
+#'   chr1 SE exon 17233 17742 . - .
+#'   chr1 SE exon 17915 18061 . - .
+#'   chr1 SE mRNA 16854 18061 . - .
+#'   chr1 SE exon 16854 17955 . - .
+#'   chr1 SE exon 17915 18061 . - .")
 #' parseMisoEvent(event)
 parseMisoEvent <- function(event) {
   event_attrs <- list("Program" = "MISO",
@@ -82,6 +117,41 @@ parseMisoEvent <- function(event) {
   return(c(event_attrs, parsed))
 }
 
+#' Parse junctions of an event from MISO according to event type
+#'
+#' @inheritParams parseMisoEvent
+#' @param strand Character: strand of the event
+#' @param parsed Named list with NAs (for faster execution)
+#'
+#' @details The following event types are available to parse MISO events:
+#' \itemize{
+#'  \item{\bold{SE} (exon skipping)}
+#'  \item{\bold{MXE} (mutually exclusive exon)}
+#'  \item{\bold{RI} (intron retention)}
+#'  \item{\bold{A5SS} (alternative 5' splice site)}
+#'  \item{\bold{A3SS} (alternative 3' splice site)}
+#'  \item{\bold{AFE} (alternative first exon)}
+#'  \item{\bold{ALE} (alternative last exon)}
+#'  \item{\bold{Tandem UTR}}
+#' }
+#' 
+#' @seealso \code{\link{parseMisoEvent}}
+#'
+#' @return List of parsed junctions
+#' @export
+#'
+#' @examples
+#' # skipping exon event (SE)
+#' event <- read.table(text = "
+#'   chr1 SE gene 16854 18061 . - .
+#'   chr1 SE mRNA 16854 18061 . - .
+#'   chr1 SE exon 16854 17055 . - .
+#'   chr1 SE exon 17233 17742 . - .
+#'   chr1 SE exon 17915 18061 . - .
+#'   chr1 SE mRNA 16854 18061 . - .
+#'   chr1 SE exon 16854 17955 . - .
+#'   chr1 SE exon 17915 18061 . - .")
+#' parseMisoSE(event, strand = "-", parsed = list())
 parseMisoSE <- function(event, strand, parsed) {
   if (strand == "+") {
     parsed[c("C1 start", "C1 end")] <- event[3, 4:5]
@@ -95,6 +165,22 @@ parseMisoSE <- function(event, strand, parsed) {
   return(parsed)
 }
 
+#' @rdname parseMisoSE
+#' 
+#' @examples
+#' 
+#' # mutually exclusive exon (MXE) event
+#' event <- read.table(text = "
+#'  chr1 MXE gene 764383 788090 . + .
+#'  chr1 MXE mRNA 764383 788090 . + .
+#'  chr1 MXE exon 764383 764484 . + .
+#'  chr1 MXE exon 776580 776753 . + .
+#'  chr1 MXE exon 787307 788090 . + .
+#'  chr1 MXE mRNA 764383 788090 . + .
+#'  chr1 MXE exon 764383 764484 . + .
+#'  chr1 MXE exon 783034 783186 . + .
+#'  chr1 MXE exon 787307 788090 . + .")
+#' parseMisoMXE(event, strand = "+", parsed = list())
 parseMisoMXE <- function(event, strand, parsed) {
   if (strand == "+") {
     parsed[c("C1 start", "C1 end")] <- event[3, 4:5]
@@ -110,6 +196,19 @@ parseMisoMXE <- function(event, strand, parsed) {
   return(parsed)
 }
 
+#' @rdname parseMisoSE
+#' 
+#' @examples
+#'
+#' # intron retention (RI) event
+#' event <- read.table(text = "
+#'  chr1 RI gene 17233 17742 . - .
+#'  chr1 RI mRNA 17233 17742 . - .
+#'  chr1 RI exon 17233 17742 . - .
+#'  chr1 RI mRNA 17233 17742 . - .
+#'  chr1 RI exon 17233 17364 . - .
+#'  chr1 RI exon 17601 17742 . - .")
+#' parseMisoRI(event, strand = "-", parsed = list())
 parseMisoRI <- function(event, strand, parsed) {
   if (strand == "+") {
     parsed[c("C1 start", "C1 end")] <- event[5, 4:5]
@@ -121,6 +220,19 @@ parseMisoRI <- function(event, strand, parsed) {
   return(parsed)
 }
 
+#' @rdname parseMisoSE
+#' @examples
+#'
+#' # alternative 5' splice site (A5SS) event
+#' event <- read.table(text = "
+#'  chr1 A5SS gene 17233 17742 . - .
+#'  chr1 A5SS mRNA 17233 17742 . - .
+#'  chr1 A5SS exon 17233 17368 . - .
+#'  chr1 A5SS exon 17526 17742 . - .
+#'  chr1 A5SS mRNA 17233 17742 . - .
+#'  chr1 A5SS exon 17233 17368 . - .
+#'  chr1 A5SS exon 17606 17742 . - .")
+#' parseMisoA5SS(event, strand = "-", parsed = list())
 parseMisoA5SS <- function(event, strand, parsed) {
   if (strand == "+") {
     parsed[["C1 start"]] <- event[3, 4]
@@ -134,6 +246,19 @@ parseMisoA5SS <- function(event, strand, parsed) {
   return(parsed)
 }
 
+#' @rdname parseMisoSE
+#' @examples
+#'
+#' # alternative 3' splice site (A3SS) event
+#' event <- read.table(text = "
+#'  chr1 A3SS gene 15796 16765 . - .
+#'  chr1 A3SS mRNA 15796 16765 . - .
+#'  chr1 A3SS exon 15796 15947 . - .
+#'  chr1 A3SS exon 16607 16765 . - .
+#'  chr1 A3SS mRNA 15796 16765 . - .
+#'  chr1 A3SS exon 15796 15942 . - .
+#'  chr1 A3SS exon 16607 16765 . - .")
+#' parseMisoA3SS(event, strand = "-", parsed = list())
 parseMisoA3SS <- function(event, strand, parsed) {
   if (strand == "+") {
     parsed[c("C1 start", "C1 end")] <- event[3, 4:5]
@@ -147,47 +272,19 @@ parseMisoA3SS <- function(event, strand, parsed) {
   return(parsed)
 }
 
-#' Get a list of each mRNA and respective exons
-list_mRNA <- function(event) {
-  mRNA_index <- which(event[ , 3] == "mRNA")
-  if (length(mRNA_index) == 1) {
-    next_index <- nrow(event)  
-  } else {
-    next_index <- c(mRNA_index[2:length(mRNA_index)] - 1, nrow(event))
-  }
-  
-  # Get each mRNA and respective exons as a new element of a list
-  mRNA <- lapply(1:(length(mRNA_index)), function(i)
-    return(event[mRNA_index[i]:next_index[i], 1:8])
-  )
-  return(mRNA)
-}
-
-#' Clear mRNAs with the same exons from a given list of mRNAs
-remove_duplicated_mRNA <- function (mRNA) {
-  # Get first occurence of each mRNA and remove duplicated index
-  uniq <- unique(match(mRNA, mRNA))
-  
-  # Return a non-redundant list of mRNAs
-  return(mRNA[uniq])
-}
-
-#' Remove wrong mRNAs of a given event
-remove_wrong_mRNA <- function(event) {
-  # Clear mRNA and exons with different chromosome identifier
-  chr      <- event[1, 1]
-  same_chr <- event[ , 1] == chr
-  event <- event[same_chr, ]
-  
-  # Clear mRNA and exons outside event boundaries
-  start  <- event[1, 4]
-  end    <- event[1, 5]
-  inside <- event[ , 4] >= start & event[ , 5] <= end
-  event  <- event[inside, ]
-  
-  return(event)
-}
-
+#' @rdname parseMisoSE
+#' @examples
+#'
+#' # alternative first exon (AFE) event
+#' event <- read.table(text = "
+#'  chr12 AFE gene 57916659 57920171  .  +  .
+#'  chr12 AFE mRNA 57919131 57920171  .  +  .
+#'  chr12 AFE exon 57919131 57920171  .  +  .
+#'  chr12 AFE mRNA 57916659 57918199  .  +  .
+#'  chr12 AFE exon 57916659 57916794  .  +  .
+#'  chr12 AFE exon 57917812 57917875  .  +  .
+#'  chr12 AFE exon 57918063 57918199  .  +  .")
+#' parseMisoAFE(event, strand = "+", parsed = list())
 parseMisoAFE <- function(event, strand, parsed) {
   # Remove mRNAs from different chromosomes and mark event
   len <- nrow(event)
@@ -248,6 +345,17 @@ parseMisoAFE <- function(event, strand, parsed) {
   return(parsed)
 }
 
+#' @rdname parseMisoSE
+#' @examples
+#'
+#' # alternative last exon (ALE) event
+#' event <- read.table(text = "
+#'  chr6 ALE gene 30620579 30822593  .  +  .
+#'  chr6 ALE mRNA 30822190 30822593  .  +  .
+#'  chr6 ALE exon 30822190 30822593  .  +  .
+#'  chr6 ALE mRNA 30620579 30620982  .  +  .
+#'  chr6 ALE exon 30620579 30620982  .  +  .")
+#' parseMisoALE(event, strand = "+", parsed = list())
 parseMisoALE <- function(event, strand, parsed) {
   len <- nrow(event)
   if (len == 1) {
@@ -315,6 +423,17 @@ parseMisoALE <- function(event, strand, parsed) {
   return(parsed)
 }
 
+#' @rdname parseMisoSE
+#' @examples
+#'
+#' # Tandem UTR event
+#' event <- read.table(text = "
+#'  chr19 TandemUTR gene  10663759  10664625  .  -  .
+#'  chr19 TandemUTR mRNA  10663759  10664625  .  -  .
+#'  chr19 TandemUTR exon  10663759  10664625  .  -  .
+#'  chr19 TandemUTR mRNA  10664223  10664625  .  -  .
+#'  chr19 TandemUTR exon  10664223  10664625  .  -  .")
+#' parseMisoTandemUTR(event, strand = "-", parsed = list())
 parseMisoTandemUTR <- function(event, strand, parsed) {
   if (strand == "+") {
     parsed[["C2 start"]] <- event[3, 4]
@@ -324,4 +443,86 @@ parseMisoTandemUTR <- function(event, strand, parsed) {
     parsed[["C2 end"]]   <- event[c(3, 5), 4]
   }
   return(parsed)
+}
+
+#' Get a list of each mRNA and respective exons
+#' 
+#' From a given alternative splicing event, put each mRNA and respective exons
+#' in a list to be returned.
+#' 
+#' @inheritParams parseMisoEvent
+#' 
+#' @return List of mRNAs and respective exons
+list_mRNA <- function(event) {
+  mRNA_index <- which(event[ , 3] == "mRNA")
+  if (length(mRNA_index) == 1) {
+    next_index <- nrow(event)  
+  } else {
+    next_index <- c(mRNA_index[2:length(mRNA_index)], nrow(event) + 1)
+  }
+  
+  # Get each mRNA and respective exons as a new element of a list
+  mRNA <- lapply(1:(length(mRNA_index)), getRowsData,
+                 event, mRNA_index, next_index - 1)
+  return(mRNA)
+}
+
+#' Clear mRNAs with the same exons from a given list of mRNAs and respective
+#' exons
+#' 
+#' For a given list where each element contains one mRNA and respective exons,
+#' compare if there are identical elements and only return each unique element
+#' once.
+#' 
+#' @param mRNA List of mRNAs and respective exons
+#' 
+#' @return Non-redundant list of mRNAs and respective exons
+remove_duplicated_mRNA <- function (mRNA) {
+  # Get first occurence of each mRNA and remove duplicated index
+  uniq <- unique(match(mRNA, mRNA))
+  
+  # Return a non-redundant list of mRNAs
+  return(mRNA[uniq])
+}
+
+#' Remove wrong mRNAs of a given alternative splicing event
+#'
+#' For a given event, checks if each mRNA and respective exons are located in 
+#' the same chromosome and in the same region as the alternative splicing event
+#' (this information is in the first row of the data frame)
+#'
+#' @inheritParams parseMisoEvent
+#' 
+#' @return Data frame with incorrect rows removed
+#' @examples
+#' # skipping exon (SE) event with incorrect mRNAs
+#' event <- read.table(text = "
+#'   chr1 SE gene 16854	18061	. - .
+#'   chr1 SE mRNA 16854 18061 . - .
+#'   chr1 SE exon 16854 17055 . - .
+#'   chr1 SE exon 17233 17742 . - .
+#'   chr1 SE exon 17915 18061 . - .
+#'   chr1 SE mRNA 16854 18061 . - .
+#'   chr1 SE exon 16854 17955 . - .
+#'   chr1 SE exon 17915 18061 . - .
+#'   chr1 SE mRNA 10 25 . - .
+#'   chr1 SE exon 10 16 . - .
+#'   chr1 SE exon 20 25 . - .
+#'   chr7 SE mRNA 16854 18061 . - .
+#'   chr7 SE exon 16854 17955 . - .")
+remove_wrong_mRNA <- function(event) {
+  ## TODO (NunoA): join these together for faster execution
+  
+  # Clear mRNA and exons with different chromosome identifier
+  chr      <- event[1, 1]
+  same_chr <- event[ , 1] == chr
+  event <- event[same_chr, ]
+  
+  # Clear mRNA and exons outside event boundaries
+  start  <- event[1, 4]
+  end    <- event[1, 5]
+  inside <- event[ , 4] >= start & event[ , 5] <= end
+  event  <- event[inside, ]
+  
+  return(event)
 }
