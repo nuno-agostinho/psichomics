936c6db65d4922d2452b4fe264213da602379876
Nuno Agostinho <nunodanielagostinho@gmail.com>
2015-12-29 02:12:47 +0000

    Event representation changed for SUPPA events

--- R/suppaEvents.R
+++ R/suppaEvents.R
@@ -1,9 +1,24 @@
-#' Parses splicing event IDs from SUPPA
-#' 
+#' Parses splicing events of a specific event type from SUPPA
+#'
 #' @details More information about SUPPA available at
 #' \url{https://bitbucket.org/regulatorygenomicsupf/suppa}
 #' 
-#' @param event Character: Splicing event ID
+#' @param event Character vector: Splicing event attributes and junction
+#' positions
+#' @param event_type Character: Alternative splicing event type (see details)
+#'
+#' @details The following event types are available to be parsed:
+#' \itemize{
+#'  \item{\bold{SE} (exon skipping)}
+#'  \item{\bold{RI} (intron retention)}
+#'  \item{\bold{MX} (mutually exclusive exons)}
+#'  \item{\bold{A5} (alternative 5' splice site)}
+#'  \item{\bold{A3} (alternative 3' splice site)}
+#'  \item{\bold{AL} (alternative last exon)}
+#'  \item{\bold{AF} (alternative first exon)}
+#' }
+#'
+#' @note It only allows to parse one event type at once.
 #'
 #' @return List with the event attributes (chromosome, strand, event type and
 #' the position of the exon boundaries)
@@ -11,71 +26,54 @@
 #'
 #' @examples
 #' event <- "ENSG00000000419;A3:20:49557492-49557642:49557470-49557642:-"
-#' parseSuppaEventID(event)
-#' 
-#' events <- c("ENSG00000000419;A3:20:49557492-49557642:49557470-49557642:-",
-#'             "ENSG00000000419;A3:20:49557492-49558568:49557470-49558568:-",
-#'             "ENSG00000000003;A5:X:99890743-99891188:99890743-99891605:-")
-#' parseSuppaEventID(events)
-parseSuppaEventID <- function(event, progress=FALSE) {
-    if (progress)
-        pb <- txtProgressBar(min=1, max=length(event), style=3)
-    # Split event ID by semicolon and colon symbols
-    event <- strsplit(event, ";|:")
-    # Create a list of lists containing event information
-    res <- lapply(1:length(event), function(e) {
-        if (progress) setTxtProgressBar(pb, e)
-        parseSuppaEvent(event[[e]])
-    })
-    return(res)
-}
-
-#' Parses splicing event from SUPPA
-#'
-#' @details More information about SUPPA available at
-#' \url{https://bitbucket.org/regulatorygenomicsupf/suppa}
-#' 
-#' @param event Character vector: Splicing event attributes and junction
-#' positions
-#'
-#' @return List with the event attributes (chromosome, strand, event type and
-#' the position of the exon boundaries)
-#' @export
-#'
-#' @examples
-#' event <- c("ENSG00000000419", "A3", "20",
-#'            "49557492-49557642", "49557470-49557642", "-")
 #' parseSuppaEvent(event)
 parseSuppaEvent <- function(event) {
-    len <- length(event)
+    # Split event ID by semicolon and colon symbols
+    event <- stringr::str_split_fixed(event, pattern = ";|:|-", n = 12)
+    event <- data.frame(event, stringsAsFactors = FALSE)
+    
     # Create list with event attributes
-    event_attrs <- list("Program" = "SUPPA",
-                        "Gene" = event[1],
-                        "Chromosome" = event[3],
-                        "Strand" = event[len])
+    event_attrs <- data.frame("Program" = "SUPPA",
+                              "Gene" = event[[1]],
+                              "Chromosome" = event[[3]],
+                              stringsAsFactors = FALSE)
     
-    type <- event[2]
-    event_attrs[["Event type"]] <- switch(type,
-                                          "MX" = "MXE",
-                                          "A5" = "A5SS",
-                                          "A3" = "A3SS",
-                                          "AF" = "AFE",
-                                          "AL" = "ALE",
-                                          type)
+    event_type <- event[1, 2]
+    # Get index of strand (depends on event type)
+    strand <- switch(event_type,
+                     "SE" = 8, "MX" = 12, "A5" = 8, "A3" = 8,
+                     "AF" = 10, "AL" = 10, "RI" = 8)
     
+    event_attrs[["Event.type"]] <- switch(event_type,
+                                          "SE"="SE",   "MX"="MXE",
+                                          "A5"="A5SS", "A3"="A3SS",
+                                          "AF"="AFE",  "AL"="ALE",
+                                          "RI"="RI")
+    
+    event_attrs[["Strand"]] <- ifelse(event[[strand]] == "+", "+", "-")
     # Get the junction positions for each exon and parse them
-    junctions <- event[4:(len-1)]
-    parsed_junctions <- parseSuppaJunctions(event_attrs[["Event type"]],
-                                            event_attrs[["Strand"]],
-                                            junctions)
-    return(c(event_attrs, parsed_junctions))
+    junctions <- event[4:(strand-1)]
+    parsed_junctions <- parseSuppaJunctions(event_attrs[["Event.type"]][[1]],
+                                            event[[strand]], junctions)
+    return(cbind(event_attrs, parsed_junctions))
 }
 
 #' Parses splicing junctions from SUPPA
 #' 
-#' @param event_type Character: Type of the splicing event
+#' @param event_type Character: Type of the splicing event (see details)
 #' @param strand Character: Strand (+ or -)
 #' @param junctions Character vector: Splicing junctions 
+#'
+#' @details The following event types are available to be parsed:
+#' \itemize{
+#'  \item{\bold{SE} (exon skipping)}
+#'  \item{\bold{RI} (intron retention)}
+#'  \item{\bold{MXE} (mutually exclusive exons)}
+#'  \item{\bold{A5SS} (alternative 5' splice site)}
+#'  \item{\bold{A3SS} (alternative 3' splice site)}
+#'  \item{\bold{ALE} (alternative last exon)}
+#'  \item{\bold{AFE} (alternative first exon)}
+#' }
 #'
 #' @return List of parsed junctions
 #' @export
@@ -84,21 +82,16 @@ parseSuppaEvent <- function(event) {
 #' allowed in some of the boundaries. This is not accounted at the moment.
 #'
 #' @examples
-#' junctionsA5 <- c("99890743-99891188", "99890743-99891605")
-#' parseSuppaJunctions(event_type = "A5", strand = "-", junctions = junctionsA5)
+#' junctions <- c("99890743-99891188", "99890743-99891605")
+#' parseSuppaJunctions(event_type = "A5SS", strand = "-", junctions = junctions)
 parseSuppaJunctions <- function(event_type, strand, junctions) {
-    # Split junctions by the hyphen
-    junctions <- strsplit(junctions, "-")
-    junctions <- as.numeric(unlist(junctions))
-    
-    # If minus strand, reverse junctions
-    if(strand == "-") junctions <- rev(junctions)
-    
     # Fill list of parsed junctions with NAs
-    parsed = list("C1 start" = NA, "C1 end" = NA,
-                  "A1 start" = NA, "A1 end" = NA,
-                  "A2 start" = NA, "A2 end" = NA,
-                  "C2 start" = NA, "C2 end" = NA)
+    parsed <- as.data.frame(matrix(NA, nrow = nrow(junctions), ncol = 8),
+                            stringsAsFactors = FALSE)
+    names(parsed) <- c("C1.start", "C1.end",
+                       "A1.start", "A1.end",
+                       "A2.start", "A2.end",
+                       "C2.start", "C2.end")
     
     # Parse junction positions according to event type
     parseJunctions <- switch(event_type,
@@ -138,10 +131,16 @@ parseSuppaJunctions <- function(event_type, strand, junctions) {
 #' @examples
 #' junctions <- c(169768099, 169770024, 169770112, 169771762)
 #' parseSuppaSE(junctions, "+")
-parseSuppaSE <- function (junctions, strand, parsed=list()) {
-    parsed[c("C1 end",
-             "A1 start", "A1 end",
-             "C2 start")] <- junctions
+parseSuppaSE <- function (junctions, strand, parsed=data.frame("C1.end"=NA)) {
+    ifelse (strand == "+",
+            { # if strand is plus
+                parsed[c("C1.end",
+                         "A1.start", "A1.end",
+                         "C2.start")] <- junctions},
+            { # if strand is minus
+                parsed[c("C2.start",
+                         "A1.end", "A1.start",
+                         "C1.end")] <- junctions})
     return(parsed)
 }
 
@@ -150,9 +149,14 @@ parseSuppaSE <- function (junctions, strand, parsed=list()) {
 #' 
 #' junctions <- c(196709749, 196709922, 196711005, 196711181)
 #' parseSuppaRI(junctions, "+")
-parseSuppaRI <- function (junctions, strand, parsed=list()) {
-    parsed[c("C1 start", "C1 end",
-             "C2 start", "C2 end")] <- junctions
+parseSuppaRI <- function (junctions, strand, parsed=data.frame("C1.end"=NA)) {
+    ifelse (strand == "+",
+            { # if strand is plus
+                parsed[c("C1.start", "C1.end",
+                         "C2.start", "C2.end")] <- junctions},
+            { # if strand is minus
+                parsed[c("C2.end", "C2.start",
+                         "C1.end", "C1.start")] <- junctions})
     return(parsed)
 }
 
@@ -161,10 +165,16 @@ parseSuppaRI <- function (junctions, strand, parsed=list()) {
 #' 
 #' junctions <- c(24790610, 24792494, 24792800, 24790610, 24795476, 24795797)
 #' parseSuppaALE(junctions, "+")
-parseSuppaALE <- function (junctions, strand, parsed=list()) {
-    parsed[c("C2 start", "C2 end",
-             "C1 end",
-             "A1 start", "A1 end")] <- junctions[2:6]
+parseSuppaALE <- function (junctions, strand, parsed=data.frame("C1.end"=NA)) {
+    ifelse (strand == "+",
+            { # if strand is plus
+                parsed[c("C2.start", "C2.end",
+                         "C1.end",
+                         "A1.start", "A1.end")] <- junctions[2:6]},
+            { # if strand is minus
+                parsed[c("C2.start", "C2.end",
+                         "C1.end",
+                         "A1.start", "A1.end")] <- junctions[5:1]})
     return(parsed)
 }
 
@@ -174,10 +184,16 @@ parseSuppaALE <- function (junctions, strand, parsed=list()) {
 #' junctions <- c(169763871, 169764046, 169767998, 169764550, 169765124,
 #'                169767998)
 #' parseSuppaAFE(junctions, "+")
-parseSuppaAFE <- function (junctions, strand, parsed=list()) {
-    parsed[c("C1 start", "C1 end",
-             "C2 start",
-             "A1 start", "A1 end")] <- junctions[1:5]
+parseSuppaAFE <- function (junctions, strand, parsed=data.frame("C1.end"=NA)) {
+    ifelse (strand == "+",
+            { # if strand is plus
+                parsed[c("C1.start", "C1.end",
+                         "C2.start",
+                         "A1.start", "A1.end")] <- junctions[1:5]},
+            { # if strand is minus
+                parsed[c("C1.start", "C1.end",
+                         "C2.start",
+                         "A1.start", "A1.end")] <- junctions[6:2]})
     return(parsed)
 }
 
@@ -187,19 +203,19 @@ parseSuppaAFE <- function (junctions, strand, parsed=list()) {
 #' junctions <- c(202060671, 202068453, 202068489, 202073793, 202060671, 
 #'                202072798, 202072906, 202073793)
 #' parseSuppaMXE(junctions, "+")
-parseSuppaMXE <- function (junctions, strand, parsed=list()) {
+parseSuppaMXE <- function (junctions, strand, parsed=data.frame("C1.end"=NA)) {
     # PSI value is related to the first alternative isoform
-    if (strand == "+") {
-        parsed[c("C1 end",
-                 "A1 start", "A1 end",
-                 "A2 start", "A2 end",
-                 "C2 start")] <- junctions[-c(4,5)]
-    } else if (strand == "-"){
-        parsed[c("C1 end",
-                 "A2 start", "A2 end",
-                 "A1 start", "A1 end",
-                 "C2 start")] <- junctions[-c(4,5)]
-    }
+    ifelse (strand == "+",
+            { # if strand is plus
+                parsed[c("C1.end",
+                         "A1.start", "A1.end",
+                         "A2.start", "A2.end",
+                         "C2.start")] <- junctions[-c(4,5)]},
+            { # if strand is minus
+                parsed[c("C2.start",
+                         "A1.end", "A1.start",
+                         "A2.end", "A2.start",
+                         "C1.end")] <- junctions[-c(4,5)]})
     return(parsed)
 }
 
@@ -208,14 +224,15 @@ parseSuppaMXE <- function (junctions, strand, parsed=list()) {
 #' 
 #' junctions <- c(169772450, 169773216, 169772450, 169773253)
 #' parseSuppaA3SS(junctions, "+")
-parseSuppaA3SS <- function (junctions, strand, parsed=list()) {
-    parsed[["C1 end"]] <- junctions[1]
-    # PSI value is related to the first alternative isoform
-    if (strand == "+") {
-        parsed[["C2 start"]] <- junctions[c(2, 4)]
-    } else if (strand == "-") {
-        parsed[["C2 start"]] <- junctions[c(4, 2)]
-    }
+parseSuppaA3SS <- function (junctions, strand, parsed=data.frame("C1.end"=NA)) {
+    # inclusion value is related to the first alternative isoform
+    ifelse (strand == "+",
+            { # if strand is plus
+                parsed[["C1.end"]] <- junctions[[1]]
+                parsed[["C2.start"]] <- apply(junctions[c(2, 4)], 1, as.list)},
+            { # if strand is minus
+                parsed[["C1.end"]] <- junctions[[4]]
+                parsed[["C2.start"]] <- apply(junctions[c(1, 3)], 1, as.list)})
     return(parsed)
 }
 
@@ -224,13 +241,14 @@ parseSuppaA3SS <- function (junctions, strand, parsed=list()) {
 #' 
 #' junctions <- c(99890743, 99891188, 99890743, 99891605)
 #' parseSuppaA5SS(junctions, "+")
-parseSuppaA5SS <- function (junctions, strand, parsed=list()) {
+parseSuppaA5SS <- function (junctions, strand, parsed=data.frame("C1.end"=NA)) {
     # PSI value is related to the first alternative isoform
-    if (strand == "+") {
-        parsed[["C1 end"]]   <- junctions[c(1, 3)]
-    } else if (strand == "-") {
-        parsed[["C1 end"]]   <- junctions[c(3, 1)]
-    }
-    parsed[["C2 start"]] <- junctions[2]
+    ifelse (strand == "+",
+            { # if strand is plus
+                parsed[["C2.start"]] <- junctions[[2]]
+                parsed[["C1.end"]] <- apply(junctions[c(1, 3)], 1, as.list)},
+            { # if strand is minus
+                parsed[["C2.start"]] <- junctions[[3]]
+                parsed[["C1.end"]] <- apply(junctions[c(2, 4)], 1, as.list)})
     return(parsed)
 }
