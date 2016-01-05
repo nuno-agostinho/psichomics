b6f63d8da76dc779b0716dc014844e8ad5f427ae
Nuno Agostinho <nunodanielagostinho@gmail.com>
2016-01-05 14:14:59 +0000

    Added some files to test Shiny app

---
+++ R/widgets.R
@@ -0,0 +1,66 @@
+name <- "Widgets"
+
+ui <- tabPanel(
+    name,
+    fluidRow(
+        bsAlert("alert"),
+        shiny::column(3, h3("Buttons"),
+                      actionButton("action", label = "Action")),
+        #br(), br(), 
+        #submitButton("Submit")), # DOESN'T ALLOW TO UPDATE VALUES...
+        shiny::column(3, h3("Single checkbox"),
+                      checkboxInput("checkbox", label = "Choice A",
+                                    value = TRUE)),
+        shiny::column(3, checkboxGroupInput(
+            "checkGroup", label = h3("Checkbox group"), 
+            choices = list("Choice 1" = 1, "Choice 2" = 2,
+                           "Choice 3" = 3), selected = 1)),
+        shiny::column(3, dateInput("date", label = h3("Date input"),
+                                   value = "2014-01-01"))   
+    ),
+    fluidRow(
+        shiny::column(3,dateRangeInput("dates", label = h3("Date range"))),
+        shiny::column(3,fileInput("file", label = h3("File input"))),
+        shiny::column(3, h3("Help text"),
+                      helpText("Note: help text isn't a true widget,", 
+                               "but it provides an easy way to add text to",
+                               "accompany other widgets.")),
+        shiny::column(3, numericInput("num", label = h3("Numeric input"),
+                                      value = 1))   
+    ),
+    fluidRow(
+        shiny::column(3, radioButtons(
+            "radio", label = h3("Radio buttons"),
+            choices = list("Choice 1" = 1,
+                           "Choice 2" = 2,
+                           "Choice 3" = 3), selected = 1)),
+        shiny::column(3, selectInput(
+            "select", label = h3("Select box"),
+            choices = list("Choice 1" = 1,
+                           "Choice 2" = 2,
+                           "Choice 3" = 3), selected = 1)),
+        shiny::column(3,
+                      sliderInput("slider1", label = h3("Sliders"), min = 0,
+                                  max = 100, value = 50),
+                      sliderInput("slider2", "", min = 0, max = 100,
+                                  value = c(25, 75))),
+        shiny::column(3, textInput("text", label = h3("Text input"), 
+                                   value = "Enter text..."))   
+    )
+)
+
+server <- function(input, output, session) {
+    obsB <- observe({
+        # dismiss same alert or it won't update
+        closeAlert(session, "exampleAlert")
+        if (input$slider1 > 90)
+            createAlert(session, "alert", "exampleAlert", title = "Wow",
+                        content = "Over 90!", style = "success")
+        else if (input$slider1 > 75)
+            createAlert(session, "alert", "exampleAlert", title = "Great",
+                        content = "Above 75!")
+        else if (input$slider1 < 25)
+            createAlert(session, "alert", "exampleAlert", title = "Too low",
+                        content = "Turn it up a bit, please.", style = "danger")
+    })
+}
