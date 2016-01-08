8e4d32d3aab5d557e9718e6ad842d48758941770
Nuno Agostinho <nunodanielagostinho@gmail.com>
2016-01-08 19:08:50 +0000

    Some changes to the project location

---
+++ R/events.R
@@ -0,0 +1,40 @@
+#' Creates a data frame of parsed junctions filled with NAs
+#' 
+#' @param nrow Integer: Number of rows
+#' @param program Character: Program used to get the junctions
+#' @param event.type Character: Event type of the respective events
+#' @param chromosome Character: Chromosome of the junctions
+#' @param strand Strand: positive ("+") or negative ("-") strand of the event
+#' 
+#' @return A data frame with the junctions coordinate names pre-filled with NAs
+#' @export
+#' 
+#' @examples
+#' createFilledJunctions(nrow = 8)
+createFilledJunctions <- function(nrow, program = character(0),
+                                  event.type = character(0),
+                                  chromosome = character(0),
+                                  strand = character(0)) {
+    parsed <- as.data.frame(matrix(NA, nrow = nrow, ncol = 8),
+                            stringsAsFactors = FALSE)
+    names(parsed) <- c("C1.start", "C1.end",
+                       "A1.start", "A1.end",
+                       "A2.start", "A2.end",
+                       "C2.start", "C2.end")
+    
+    if (length(program) > 0) parsed[["Program"]] <- "MISO"
+    if (length(event.type) > 0) parsed[["Event.type"]] <- event.type
+    if (length(chromosome) > 0) parsed[["Chromosome"]] <- chromosome
+    if (length(strand) > 0) parsed[["Strand"]] <- strand
+    return(parsed)
+}
+
+#' Converts a vector of numeric characters to a list of vectors of integers
+#'
+#' @param i Numeric character vector
+#'
+#' @return List of numeric vectors
+#' @export
+numericList <- function(i) {
+    return(as.list(as.numeric(i)))
+}
