4cbd267fb2ac2c4baf70f002d3783bd02bebe9e8
Nuno Agostinho <nunodanielagostinho@gmail.com>
2016-02-07 15:39:42 +0000

    Documentation and testing of the Organism class

--- R/organism.R
+++ R/organism.R
@@ -1,3 +1,21 @@
+#' Organism representation
+#'
+#' @slot species Character: species name
+#' @slot common.name Character: common name
+#' @slot clinical.information Data.frame: clinical information
+#' @slot inclusion.levels Data.frame: inclusion levels of a given exon
+#' @export
+#'
+#' @examples
+#' # create a new organism object ('Homo sapiens' by default)
+#' org <- new("Organism") 
+#' 
+#' # create a new organism object (this time a mouse)
+#' org <- new("Organism", species = "Mus musculus", common.name = "Mouse")
+#' 
+#' # string representation of the object (displays the species and respective
+#' # common name of a organism)
+#' as(org, "character")
 setClass("Organism",
          representation = representation(
              species = "character",
@@ -8,6 +26,7 @@ setClass("Organism",
              species = "Homo sapiens",
              common.name = "Human"))
 
+# Checks validity of Organism's attributes
 setValidity("Organism",
             function(object) {
                 if (length(object@species) != 1) 
@@ -20,32 +39,45 @@ setValidity("Organism",
                     TRUE
             })
 
+# String representation of the object
 setAs("Organism", "character", function(from)
     if (from@common.name != "")
         sprintf("%s (%s)", from@species, from@common.name)
     else
         sprintf("%s", from@species))
 
+
+#' Gets the inclusion levels of an organism
+#'
+#' @param object Organism
+#'
+#' @return Inclusion levels of an organism
+#' @export
+#'
+#' @examples
+#' org <- new("Organism",
+#'            inclusion.levels = data.frame(1,2,3))
+#' inclusion.levels(org)
 setGeneric("inclusion.levels", function(object)
     standardGeneric("inclusion.levels"))
+
+#' @rdname inclusion.levels
 setMethod("inclusion.levels", "Organism", function(object)
     object@inclusion.levels)
 
-setGeneric("inclusion.levels<-", function(object, value)
+#' Sets the inclusion levels of an organism
+#'
+#' @param object Organism
+#' @param inclusion.levels Data.frame
+#' @export
+#'
+#' @examples
+#' org <- new("Organism")
+#' inclusion.levels(org) <- data.frame(1,2,3)
+setGeneric("inclusion.levels<-", function(object, inclusion.levels)
     standardGeneric("inclusion.levels<-"))
-setReplaceMethod("inclusion.levels", "Organism", function(object, value)
-    initialize(object, inclusion.levels = value))
 
-# -----------------------------------------------------------------------
-# org <- new("Organism",
-#            species = "Mus musculus",
-#            common.name = "Mouse")
-# as(org, "character")
-# inclusion.levels(org)
-# inclusion.levels(org) <- data.frame(1,2,3)
-# inclusion.levels(org)
-# 
-# org <- new("Organism",
-#            species = "Mus musculus",
-#            common.name = "Mouse",
-#            inclusion.levels = data.frame(1,2,3))
+#' @rdname inclusion.levels-set
+setReplaceMethod("inclusion.levels", "Organism",
+                 function(object, inclusion.levels)
+                     initialize(object, inclusion.levels = inclusion.levels))
