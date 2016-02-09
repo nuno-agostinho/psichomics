d98634a0cf67c4010427c693d119751676668f32
Nuno Agostinho <nunodanielagostinho@gmail.com>
2016-02-09 18:16:39 +0000

    Added datatable

---
+++ R/plots/datatable.R
@@ -0,0 +1,28 @@
+# The name used for the plot must be unique
+name <- "datatable"
+
+ui <- list(
+    dataTableOutput(name)
+)
+
+createLink <- function(val) {
+    a <- '<a id="%s" href="#">%s</button>'
+    sprintf(a, gsub(" ", "_", val), val)
+    #actionButton(val[1], val[1])
+}
+
+server <- function(input, output, session) {
+    output[[name]] <- renderDataTable({
+        tmp <- cbind(cars = createLink(rownames(mtcars)),
+                     mtcars)
+        tmp
+    }, escape = FALSE, options = list(pageLength = 10))
+    
+    lapply(gsub(" ", "_", rownames(mtcars)), function(i) {
+        onclick(i, {
+            updateSelectizeInput(session, "selectizePlot", selected = "plot4")
+            updateSelectizeInput(session, "selectizeEvent",
+                                 selected = gsub("_", " ", i))
+        })
+    })
+}
