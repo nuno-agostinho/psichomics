2d12833836155fc3e03ddcfc07ad8ce7f880e810
Nuno Agostinho <nunodanielagostinho@gmail.com>
2016-02-16 14:05:20 +0000

    Created a generic function to promote code reuse

--- R/matsEvents.R
+++ R/matsEvents.R
@@ -63,24 +63,18 @@ parseMatsEvent <- function(event, event_type) {
     }
 }
 
-# parseMatsGeneric <- function(junctions, strand, coords, plus_pos, minus_pos) {
-#     # Creates a data frame of parsed junctions filled with NAs
-#     parsed <- createJunctionsTemplate(nrow(junctions))
-#     
-#     plus <- strand == "+"
-#     parsed[plus, coords] <- junctions[plus, plus_pos]       # Plus strand
-#     parsed[!plus, coords] <- junctions[!plus, minus_pos]    # Minus strand
-#     return(parsed)
-# }
-
-
 #' Parse junctions of an alternative splicing event from MATS according to event 
 #' type
 #'
-#' @param junctions Vector of integers with the event's junctions
+#' @param junctions Integer: event's junctions
 #' @param strand Character: strand of the event
+#' @param coords Character: names of the alternative splicing coordinates
+#' @param plus_pos Integer: match of each junction in the respective coordinate
+#' for the plus strand
+#' @param minus_pos Integer: match of each junction in the respective coordinate
+#' for the minus strand
 #' 
-#' @details The following event types are available to be parsed:
+#' @details The following event types are ready to be parsed:
 #' \itemize{
 #'  \item{\bold{SE} (exon skipping)}
 #'  \item{\bold{MXE} (mutually exclusive exon)}
@@ -91,151 +85,141 @@ parseMatsEvent <- function(event, event_type) {
 #'  \item{\bold{ALE} (alternative last exon)}
 #' }
 #' 
-#' @seealso \code{\link{parseMisoEvent}}
+#' You can use \code{parseMatsGeneric} to parse other event types.
+#' 
+#' @seealso \code{\link{parseMatsEvent}}
 #' 
 #' @return Data frame with parsed junctions
 #' @export
 #' 
 #' @examples 
-#' junctions <- read.table(text="79685787 79685910 79685796 79685910 79679566 79679751")
-#' parseMatsSE(junctions, strand = "+")
-parseMatsSE <- function(junctions, strand) {
+#' # Parse generic event (in this case, a skipping exon event)
+#' junctions <- read.table(text=
+#'     "79685787 79685910 79685796 79685910 79679566 79679751")
+#' coords <- c("A1.start", "A1.end",
+#'             "C1.start", "C1.end",
+#'             "C2.start", "C2.end")
+#' plus  <- c(1:6)
+#' minus <- c(2:1, 6:3)
+#' parseMatsGeneric(junctions, strand = "+", coords, plus, minus)
+parseMatsGeneric <- function(junctions, strand, coords, plus_pos, minus_pos) {
     # Creates a data frame of parsed junctions filled with NAs
     parsed <- createJunctionsTemplate(nrow(junctions))
     
     plus <- strand == "+"
-    # Plus strand
-    parsed[plus, c("A1.start", "A1.end",
-                   "C1.start", "C1.end",
-                   "C2.start", "C2.end")] <- junctions[plus, 1:6]
-    # Minus strand
-    parsed[!plus, c("A1.end", "A1.start",
-                    "C2.end", "C2.start",
-                    "C1.end", "C1.start")] <- junctions[!plus, 1:6]
+    parsed[plus, coords] <- junctions[plus, plus_pos]       # Plus strand
+    parsed[!plus, coords] <- junctions[!plus, minus_pos]    # Minus strand
     return(parsed)
 }
 
-#' @rdname parseMatsSE
+#' @rdname parseMatsGeneric
 #' @examples 
 #' 
+#' # Parse skipping exon event
+#' junctions <- read.table(text=
+#'     "79685787 79685910 79685796 79685910 79679566 79679751")
+#' parseMatsSE(junctions, strand = "+")
+parseMatsSE <- function(junctions, strand) {
+    coords <- c("A1.start", "A1.end",
+                "C1.start", "C1.end",
+                "C2.start", "C2.end")
+    plus  <- c(1:6)
+    minus <- c(2:1, 6:3)
+    parseMatsGeneric(junctions, strand, coords, plus, minus)
+}
+
+#' @rdname parseMatsGeneric
+#' @examples 
+#' 
+#' # Parse mutually exclusive exon event
 #' junctions <- read.table(text=
 #' "158282161 158282276 158282689 158282804 158281047 158281295 158283950 158284199")
 #' parseMatsMXE(junctions, strand = "+")
 parseMatsMXE <- function(junctions, strand) {
-    # Creates a data frame of parsed junctions filled with NAs
-    parsed <- createJunctionsTemplate(nrow(junctions))
-    
-    plus <- strand == "+"
-    # Plus strand
-    parsed[plus, c("A1.start", "A1.end",
-                   "A2.start", "A2.end",
-                   "C1.start", "C1.end",
-                   "C2.start", "C2.end")] <- junctions[plus, 1:8]
-    # Minus strand
-    parsed[!plus, c("A1.end", "A1.start",
-                    "A2.end", "A2.start",
-                    "C2.end", "C2.start",
-                    "C1.end", "C1.start")] <- junctions[!plus, 1:8]
-    return(parsed)
+    coords <- c("A1.start", "A1.end",
+                "A2.start", "A2.end",
+                "C1.start", "C1.end",
+                "C2.start", "C2.end")
+    plus  <- c(1:8)
+    minus <- c(2:1, 4:3, 8:5)
+    parseMatsGeneric(junctions, strand, coords, plus, minus)
 }
 
-#' @rdname parseMatsSE
+#' @rdname parseMatsGeneric
 #' @examples 
 #' 
-#' junctions <- read.table(text="15929853 15932100 15929853 15930016 15930687 15932100")
+#' # Parse intron retention event
+#' junctions <- read.table(text=
+#'     "15929853 15932100 15929853 15930016 15930687 15932100")
 #' parseMatsRI(junctions, strand = "+")
 parseMatsRI <- function(junctions, strand) {
-    # Creates a data frame of parsed junctions filled with NAs
-    parsed <- createJunctionsTemplate(nrow(junctions))
-    
-    plus <- strand == "+"
-    # Plus strand
-    parsed[plus, c("C1.start", "C1.end",
-                   "C2.start", "C2.end")] <- junctions[plus, 3:6]
-    # Minus strand
-    parsed[!plus, c("C1.start", "C1.end",
-                    "C2.start", "C2.end")] <- junctions[!plus, 6:3]
-    return(parsed)
+    coords <- c("C1.start", "C1.end",
+                "C2.start", "C2.end")
+    plus  <- c(3:6)
+    minus <- c(6:3)
+    parseMatsGeneric(junctions, strand, coords, plus, minus)
 }
 
-#' @rdname parseMatsSE
+#' @rdname parseMatsGeneric
 #' @examples 
 #' 
-#' junctions <- read.table(text="79685787 79685910 79685796 79685910 79679566 79679751")
+#' # Parse alternative 3' splicing site event
+#' junctions <- read.table(text=
+#'     "79685787 79685910 79685796 79685910 79679566 79679751")
 #' parseMatsA3SS(junctions, strand = "+")
 parseMatsA3SS <- function(junctions, strand) {
-    # Creates a data frame of parsed junctions filled with NAs
-    parsed <- createJunctionsTemplate(nrow(junctions))
-    
-    # Note that inclusion values are related to the first alternative isoform
-    plus <- strand == "+"
-    # Plus strand
-    parsed[plus, c("C1.start", "C1.end", "A1.start",
-                   "C2.start", "C2.end")] <- junctions[plus, c(5:6, 3, 1, 2)]
-    # Minus strand
-    parsed[!plus, c("C1.start", "C1.end", "A1.start",
-                    "C2.start", "C2.end")] <- junctions[!plus, c(6:4, 2:1)]
-    return(parsed)
+    coords <- c("C1.start", "C1.end",
+                "A1.start",
+                "C2.start", "C2.end")
+    plus  <- c(5:6, 3, 1, 2)
+    minus <- c(6:4, 2:1)
+    parseMatsGeneric(junctions, strand, coords, plus, minus)
 }
 
-#' @rdname parseMatsSE
+#' @rdname parseMatsGeneric
 #' @examples 
 #' 
-#' junctions <- read.table(text="102884421 102884501 102884421 102884489 102884812 102885881")
+#' # Parse alternative 5' splicing site event
+#' junctions <- read.table(text=
+#'     "102884421 102884501 102884421 102884489 102884812 102885881")
 #' parseMatsA5SS(junctions, strand = "+")
 parseMatsA5SS <- function(junctions, strand) {
-    # Creates a data frame of parsed junctions filled with NAs
-    parsed <- createJunctionsTemplate(nrow(junctions))
-    
-    # Note that inclusion values are related to the first alternative isoform
-    plus <- strand == "+"
-    # Plus strand
-    parsed[plus, c("C1.start", "C1.end", "A1.end",
-                   "C2.start", "C2.end")] <- junctions[plus, c(1:2, 4:6)]
-    # Minus strand
-    parsed[!plus, c("C1.start", "C1.end", "A1.end",
-                    "C2.start", "C2.end")] <- junctions[!plus, c(2:1, 3, 6, 5)]
-    return(parsed)
+    coords <- c("C1.start", "C1.end",
+                            "A1.end",
+                "C2.start", "C2.end")
+    plus  <- c(1:2, 4:6)
+    minus <- c(2:1, 3, 6, 5)
+    parseMatsGeneric(junctions, strand, coords, plus, minus)
 }
 
-#' @rdname parseMatsSE
+#' @rdname parseMatsGeneric
 #' @examples 
 #' 
-#' junctions <- read.table(text="16308723 16308879 16308967 16309119 16314269 16314426")
+#' # Parse alternative first exon event
+#' junctions <- read.table(text=
+#'     "16308723 16308879 16308967 16309119 16314269 16314426")
 #' parseMatsAFE(junctions, strand = "+")
 parseMatsAFE <- function(junctions, strand) {
-    # Creates a data frame of parsed junctions filled with NAs
-    parsed <- createJunctionsTemplate(nrow(junctions))
-    
-    plus <- strand == "+"
-    # Plus strand
-    parsed[plus, c("C1.start", "C1.end",
-                   "A1.start", "A1.end",
-                   "C2.start", "C2.end")] <- junctions[plus, 1:6]
-    # Minus strand
-    parsed[!plus, c("C1.start", "C1.end",
-                    "A1.start", "A1.end",
-                    "C2.start", "C2.end")] <- junctions[!plus, c(2:1, 4:3, 6:5)]
-    return(parsed)
+    coords <- c("C1.start", "C1.end",
+                "A1.start", "A1.end",
+                "C2.start", "C2.end")
+    plus  <- c(1:6)
+    minus <- c(2:1, 4:3, 6:5)
+    parseMatsGeneric(junctions, strand, coords, plus, minus)
 }
 
-#' @rdname parseMatsSE
+#' @rdname parseMatsGeneric
 #' @examples 
 #' 
-#' junctions <- read.table(text="111858645 111858828 111851063 111851921 111850441 111850543")
+#' # Parse alternative last exon event
+#' junctions <- read.table(text=
+#'     "111858645 111858828 111851063 111851921 111850441 111850543")
 #' parseMatsAFE(junctions, strand = "+")
 parseMatsALE <- function(junctions, strand) {
-    # Creates a data frame of parsed junctions filled with NAs
-    parsed <- createJunctionsTemplate(nrow(junctions))
-    
-    plus <- strand == "+"
-    # Plus strand
-    parsed[plus, c("C1.start", "C1.end",
-                   "A1.start", "A1.end",
-                   "C2.start", "C2.end")] <- junctions[plus, c(5:6, 1:2, 3:4)]
-    # Minus strand
-    parsed[!plus, c("C1.start", "C1.end",
-                    "A1.start", "A1.end",
-                    "C2.start", "C2.end")] <- junctions[!plus, c(6:5, 2:1, 4:3)]
-    return(parsed)
+    coords <- c("C1.start", "C1.end",
+                "A1.start", "A1.end",
+                "C2.start", "C2.end")
+    plus  <- c(5:6, 1:2, 3:4)
+    minus <- c(6:5, 2:1, 4:3)
+    parseMatsGeneric(junctions, strand, coords, plus, minus)
 }
