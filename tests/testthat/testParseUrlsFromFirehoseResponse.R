71ac7e865ba3f6a036df3c2ecfabe38c7e5045e9
Nuno Agostinho <nunodanielagostinho@gmail.com>
2016-02-22 15:03:38 +0000

    Test added

---
+++ tests/testthat/testParseUrlsFromFirehoseResponse.R
@@ -0,0 +1,8 @@
+context("Test the parsing of URLs from a Firehose response")
+
+test_that("Parse the URLs from a Firehose response", {
+    res <- queryFirehoseData(cohort = "ACC")
+    url <- parseUrlsFromFirehoseResponse(res)
+    expect_equal(length(unique(names(url))), 1)
+    expect_true(grepl("ACC", unique(names(url))))
+})
