1b2a401014af5de48c898cce16330ae511b75b77
Nuno Agostinho <nunodanielagostinho@gmail.com>
2016-02-22 18:20:42 +0000

    Made the "format" more pretty

--- R/filesFormat/merge_clinical.R
+++ R/filesFormat/merge_clinical.R
@@ -1,12 +1,14 @@
 tablename <- "Clinical data"         # Name of the created table
-filename <- "clin.merged.txt"   # Name of the file
-delim <- "\t"                   # Delimiter used to separate fields
-transpose <- TRUE               # Tranpose data
-headerCheck <- 1                # Header row in the file to check format
-headerUse <- 1                  # Header row in the file to use as header
+filename <- "clin.merged.txt"        # Name of the file
+description <- "Clinical data of the patients"
+
+# Parsing information
+delim <- "\t"       # Delimiter used to separate fields
+transpose <- TRUE   # Tranpose data
+headerCheck <- 1    # Header row in the file to check format
+headerUse <- 1      # Header row in the file to use as header
 commentChar <- NULL # String to identify comments (these lines will be ignored)
 
-# First line of data.frame with content of interest
 # If you have a header on line 1, the content probably starts on line 2
 contentStart <- 2
 
