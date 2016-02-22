99902e2f56d72a04257a6ef8c6d036f22cb27c2d
Nuno Agostinho <nunodanielagostinho@gmail.com>
2016-02-22 18:20:42 +0000

    Made the "format" more pretty

--- R/filesFormat/junction_quantification.R
+++ R/filesFormat/junction_quantification.R
@@ -1,12 +1,14 @@
 tablename <- "Junction quantification" # Name of the created table
 filename <- "junction_quantification" # Name of the file
-delim <- "\t"                   # Delimiter used to separate fields
-transpose <- FALSE              # Tranpose data
-headerCheck <- 2                # Header row in the file to check format
-headerUse <- 1                  # Header row in the file to use as header
+description <- "Quantification of alternatively spliced junctions"
+
+# Parsing information
+delim <- "\t"       # Delimiter used to separate fields
+transpose <- FALSE  # Tranpose data
+headerCheck <- 2    # Header row in the file to check format
+headerUse <- 1      # Header row in the file to use as header
 commentChar <- NULL # String to identify comments (these lines will be ignored)
 
-# First line of data.frame with content of interest
 # If you have a header on line 1, the content probably starts on line 2
 contentStart <- 3
 
