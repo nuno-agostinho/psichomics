0d474eba96dd578d33faf9903f36d646205d2e62
Nuno Agostinho <nunodanielagostinho@gmail.com>
2016-02-24 16:09:53 +0000

    Improved mechanism of checking and loading file formats

--- R/filesFormat/merge_clinical.R
+++ R/filesFormat/merge_clinical.R
@@ -1,18 +1,24 @@
-tablename <- "Clinical data"         # Name of the created table
-filename <- "clin.merged.txt"        # Name of the file
+tablename   <- "Clinical data"      # Name of the created table
+filename    <- "clin.merged.txt"    # Name of the file
 description <- "Clinical data of the patients"
 
+# Transpose the data? This is the first step before parsing the information!
+# After transposition, a row of the current data equals a column of the original
+transpose   <- FALSE
+
+# Format checker information
+rowCheck    <- FALSE # Check format using a row (TRUE) or a column (FALSE)
+checkIndex  <- 1     # Index of the row or column used to check the format
+
 # Parsing information
-delim <- "\t"       # Delimiter used to separate fields
-transpose <- TRUE   # Tranpose data
-headerCheck <- 1    # Header row in the file to check format
-headerUse <- 1      # Header row in the file to use as header
-commentChar <- NULL # String to identify comments (these lines will be ignored)
+delim       <- "\t"  # Delimiter used to separate fields
+colNames    <- NULL  # Row to use for column names
+rowNames    <- 1     # Column to use for row names
+ignoreCols  <- 1     # Columns to ignore
+ignoreRows  <- NULL  # Rows to ignore
+commentChar <- NULL  # String to identify comments (these lines will be ignored)
 
-# If you have a header on line 1, the content probably starts on line 2
-contentStart <- 2
-
-# Header of the file
-header <- c("admin.batch_number", "admin.bcr", "admin.day_of_dcc_upload",
-            "admin.disease_code", "admin.file_uuid",
-            "admin.month_of_dcc_upload")
+# File string to check
+check <- c("admin.batch_number", "admin.bcr", "admin.day_of_dcc_upload",
+           "admin.disease_code", "admin.file_uuid",
+           "admin.month_of_dcc_upload")
