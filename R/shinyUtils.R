affdda18458566ee79bc9ff2c581cb49822967b1
Nuno Agostinho <nunodanielagostinho@gmail.com>
2016-02-26 19:30:42 +0000

    Changed shinyBS::bsModal to allow to modify the footer

--- R/shinyUtils.R
+++ R/shinyUtils.R
@@ -39,3 +39,33 @@ dropdown <- function(inputId) {
                 tags$li(a(href="#", "Something else")))
     )
 }
+
+bsModal2 <- function (id, title, trigger, ..., size, footer = NULL)  {
+    if (!missing(size)) {
+        if (size == "large") size = "modal-lg"
+        else if (size == "small") size = "modal-sm"
+        size <- paste("modal-dialog", size)
+    }
+    else size <- "modal-dialog"
+    bsTag <- shiny::tags$div(
+        class = "modal sbs-modal fade", 
+        id = id, tabindex = "-1", `data-sbs-trigger` = trigger, 
+        shiny::tags$div(
+            class = size, shiny::tags$div(
+                class = "modal-content", 
+                shiny::tags$div(
+                    class = "modal-header", shiny::tags$button(
+                        type = "button", 
+                        class = "close", `data-dismiss` = "modal",
+                        shiny::tags$span(shiny::HTML("&times;"))), 
+                    shiny::tags$h4(class = "modal-title", title)), 
+                shiny::tags$div(class = "modal-body", list(...)), 
+                shiny::tags$div(
+                    class = "modal-footer",
+                    shiny::tags$button(type = "button", 
+                                       class = "btn btn-default",
+                                       `data-dismiss` = "modal", 
+                                       "Close"),
+                    footer))))
+    htmltools::attachDependencies(bsTag, shinyBS:::shinyBSDep)
+}
