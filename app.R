95ce447452b23def7525525fb7832a99364a0b31
Nuno Agostinho <nunodanielagostinho@gmail.com>
2016-02-27 15:51:15 +0000

    Moved all CSS to a separate file

--- app.R
+++ app.R
@@ -13,31 +13,17 @@ server <- function(input, output, session) {
     
     session$onSessionEnded(function() {
         # Stop app and print message to console
-        suppressMessages(stopped <- stopApp(returnValue=TRUE))
-        if (stopped) cat("\nShiny app was closed ")
+        suppressMessages(stopped <- stopApp(returnValue="Shiny app was closed"))
     })
 }
 
-addCSS <- function(...) tags$style(type = "text/css", ...)
-
-header <- list(
-    useShinyjs(),
-    ## TODO(NunoA): put all CSS info in a CSS file and import with "includeCSS"
-    # Avoids fixed-top navbar from obscuring content
-    addCSS("body { padding-top: 70px; }"),
-    # Alert appears fixed on the top right above other elements
-    addCSS(".sbs-alert { position:fixed;
-                         right:10px;
-                         z-index:9;
-                         -webkit-filter: opacity(80%);
-                         filter: opacity(80%); }"),
-    addCSS(".shiny-progress-container { top: 48px; }"))
-
 # The user interface (ui) controls the layout and appearance of the app
+# All the CSS modifications are in the file "www/styles.css"
 ui <- shinyUI(
     do.call(navbarPage, c(
         list(title = "spliced canceR", id = "nav", collapsible = TRUE,
-             position = "fixed-top", header = header),
+             position = "fixed-top", header = includeCSS("www/styles.css"),
+             footer = useShinyjs()),
         # Loads the interface of each tab
         callScriptsFunction(func = "ui", check = c("name", "ui"), tabPanel)
     ))
