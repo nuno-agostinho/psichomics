daac6165fda6720fe72797ad628516e7ffc236ed
Nuno Agostinho <nunodanielagostinho@gmail.com>
2016-03-11 15:09:07 +0000

    Added file to calculate inclusion levels

---
+++ R/plots/inclusionLevels2.R
@@ -0,0 +1,53 @@
+# The name used for the plot must be unique
+name <- "Inclusion levels 2"
+
+ui <- list(
+    uiOutput(name)
+)
+
+createLink <- function(val) {
+    onclick <- sprintf('document.getElementById(\'%s\').selectize.setValue(\'%s\')',
+                       c("selectizePlot", "selectizeEvent"),
+                       c("plot4", "Valiant"))
+    onclick <- paste(onclick, collapse = "; ")
+    html <- paste('<a id="%s" title="Get more information about this event"',
+                  'href="#" onclick="%s">%s</a>')
+    link <- sprintf(html, gsub("_", " ", val), onclick, val)
+    return(link)
+}
+
+server <- function(input, output, session) {
+    levels <- reactive({
+        # Read annotation
+        startProgress("Reading alternative splicing annotation",
+                      divisions = 2)
+        annot <- readAnnotation("SE")
+        
+        # Calculate inclusion levels with annotation and junction quantification
+        updateProgress("Calculating inclusion levels")
+        junctionQuant <- shared.data$data[[1]][["Junction quantification"]]
+        shared.data$psi <- calculateInclusionLevels(
+            "SE", junctionQuant, annot)
+        
+        updateProgress("Done!")
+        # Close the progress even if there's an error
+        shared.data$progress$close()
+    })
+    
+    observe({
+        if(is.null(shared.data$data) ||
+           is.null(shared.data$data[[1]][["Junction quantification"]]))
+            disableTab("Plots")
+        else
+            enableTab("Plots")
+        
+        if(input$nav == 'Plots') levels()
+    })
+    
+    output[[name]] <- renderUI( dataTableOutput("incLevels") )
+    
+    output$incLevels <- renderDataTable({
+        cbind(Events = createLink(rownames(shared.data$psi)), 
+              shared.data$psi)
+    }, escape = FALSE, options = list(pageLength = 10, scrollX = TRUE))
+}
