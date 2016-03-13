a90b886f3027132d7f1f2d9d32255528efbad42e
Nuno Agostinho <nunodanielagostinho@gmail.com>
2016-03-13 23:34:13 +0000

    Renamed inclusion levels

--- R/plots/inclusionLevels.R
+++ R/plots/inclusionLevels.R
@@ -1,26 +1,51 @@
 # The name used for the plot must be unique
 name <- "Inclusion levels"
 
-ui <- list(
-    dataTableOutput(name)
-)
+ui <- list( uiOutput(name) )
 
 createLink <- function(val) {
-    a <- '<a id="%s" href="#">%s</button>'
-    sprintf(a, gsub(" ", "_", val), val)
-    #actionButton(val[1], val[1])
+    id <- gsub(" ", "_", val)
+    onclick <- sprintf(
+        'document.getElementById(\'%s\').selectize.setValue(\'%s\')',
+        c("selectizePlot", "selectizeEvent"),
+        c(rep("plot4", length(id)), id))
+    onclick <- paste(onclick, collapse = "; ")
+    html <- paste('<a id="%s" title="Get more information about this event"',
+                  'href="#" onclick="%s">%s</a>')
+    link <- sprintf(html, id, onclick, val)
+    return(link)
 }
 
 server <- function(input, output, session) {
-    output[[name]] <- renderDataTable({
-        cbind(cars = createLink(rownames(mtcars)), mtcars)
-    }, escape = FALSE, options = list(pageLength = 10))
-    
-    lapply(gsub(" ", "_", rownames(mtcars)), function(i) {
-        onclick(i, {
-            updateSelectizeInput(session, "selectizePlot", selected = "plot4")
-            updateSelectizeInput(session, "selectizeEvent",
-                                 selected = gsub("_", " ", i))
-        })
+    levels <- reactive({
+        # Read annotation
+        startProgress("Reading alternative splicing annotation",
+                      divisions = 2)
+        annot <- readAnnotation("SE")
+        
+        # Calculate inclusion levels with annotation and junction quantification
+        updateProgress("Calculating inclusion levels")
+        junctionQuant <- getJunctionQuantification()
+        setInclusionLevels(
+            calculateInclusionLevels("SE", junctionQuant, annot))
+        
+        updateProgress("Done!")
+        closeProgress()
     })
+    
+    observe({
+        if(is.null(getData()) || is.null(getJunctionQuantification()))
+            disableTab("Plots")
+        else
+            enableTab("Plots")
+        
+        if(input$nav == 'Plots') levels()
+    })
+    
+    output[[name]] <- renderUI( dataTableOutput("incLevels") )
+    
+    output$incLevels <- renderDataTable({
+        psi <- getInclusionLevels()
+        cbind(Events = createLink(rownames(psi)), psi)
+    }, escape = FALSE, options = list(pageLength = 10, scrollX = TRUE))
 }
