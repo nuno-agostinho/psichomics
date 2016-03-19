a4812368a615654d20f2fc3239598499eff10f92
Nuno Agostinho <nunodanielagostinho@gmail.com>
2016-03-19 16:10:25 +0000

    Added new function to make each ID unique for each module

--- R/data.R
+++ R/data.R
@@ -1,53 +1,54 @@
 #' @import shiny shinyBS shinyjs
 name <- "Data"
+id <- function(id) paste(name, id, sep = "_")
 
 #' Creates a UI set with options to add a file from the local storage
 #' 
 #' @return A UI set that can be added to a UI definition
 addLocalFile <- function() {
-    list(fileInput("dataFile", "Choose folder", multiple = T),
-         textInput("species", label = "Species", placeholder = "Required"),
-         textInput("commonName", label = "Common name"),
-         uiOutput("testing"),
-         actionButton("acceptFile", "Send file")
+    list(fileInput(id("dataFile"), "Choose folder", multiple = T),
+         textInput(id("species"), label = "Species", placeholder = "Required"),
+         textInput(id("commonName"), label = "Common name"),
+         uiOutput(id("testing")),
+         actionButton(id("acceptFile"), "Send file")
     ) # end of list
 }
 
 groupsUI <- function(table, tablename) {
     checkId <- function (sign, what)
-        sprintf("input[id='%s'] %s '%s'", "subsetBy", sign, what)
+        sprintf("input[id='%s'] %s '%s'", id("subsetBy"), sign, what)
     
     list(
         hr(),
         fluidRow(
-            column(2, selectizeInput("subsetBy", "Subset by",
+            column(2, selectizeInput(id("subsetBy"), "Subset by",
                                      c("Column", "Rows", "Expression"))),
             conditionalPanel(
                 checkId("==", "Column"),
-                column(3, selectizeInput("groupColumn", "Select column",
+                column(4, selectizeInput(id("groupColumn"), "Select column",
                                          choices = names(table))),
-                column(1, icon2("info-circle", id = "info-circle"),
-                       bsTooltip("info-circle",
+                column(1, icon2("info-circle", id = id("info-circle")),
+                       bsTooltip(id("info-circle"),
                                  "Groups will be created automatically depending on the given column."))),
             conditionalPanel(
                 checkId("==", "Rows"),
-                column(3, textInput("groupRows", "Select rows"),
-                       bsTooltip("groupRows",
+                column(3, textInput(id("groupRows"), "Select rows"),
+                       bsTooltip(id("groupRows"),
                                  "Select rows like in R. To create a group with rows 1 to 6, 8, 10 to 19, but not 17, insert 1:6, 8, 10:19, -17"))),
             conditionalPanel(
                 checkId("==", "Expression"),
-                column(3, textInput("groupExpression", "Subset expression"),
-                       bsTooltip("groupExpression",
+                column(3, textInput(id("groupExpression"), "Subset expression"),
+                       bsTooltip(id("groupExpression"),
                                  'To select rows where column X4 is higher than 8 and "alive" in X7, type X4 > 8 & X7 == "alive"'))),
             column(2, conditionalPanel(checkId("!=", "Column"),
-                                       textInput("groupName", "Group name"))),
-            column(2, actionButton("createGroup", "Create group"))),
+                                       textInput(id("groupName"), "Group name"))),
+            column(2, actionButton(id("createGroup"), "Create group"))),
         # Align the "create group" button with other inputs
         tags$style(type='text/css', paste0(
-            "#createGroup{ width:100\\%; margin-top: 25px;}")),
+            "#", id("createGroup"), " { width:100\\%; margin-top: 25px;}")),
         tags$style(type='text/css',
-                   "#info-circle { width:100\\%; margin-top: 35px;}"),
-        tableOutput("groupsTable")
+                   "#", id("icon-circle"), " { width:100\\%; margin-top: 35px;}"),
+        tableOutput(id("groupsTable"))
     )
 }
 
@@ -59,18 +60,19 @@ addTCGAdata <- function() {
     names(cohorts) <- sprintf("%s (%s)", names(cohorts), cohorts)
     
     if (isFirehoseUp()) {
-        list(selectizeInput("firehoseCohort", "Cohort", cohorts,
+        list(selectizeInput(id("firehoseCohort"), "Cohort", cohorts,
                             multiple = TRUE, selected = c("ACC", "BLCA"),
                             options = list(placeholder = "Select cohort(s)")),
-             selectizeInput("firehoseDate", "Date",
+             selectizeInput(id("firehoseDate"), "Date",
                             as.character(getFirehoseDates()), multiple = TRUE,
                             selected = "2015-11-01", options = list(
                                 placeholder = "Select sample date")),
-             selectizeInput("dataType", "Data type", c("Clinical", "mRNASeq"), 
+             selectizeInput(id("dataType"), "Data type",
+                            c("Clinical", "mRNASeq"), 
                             multiple = TRUE, selected = "Clinical",
                             options = list(
                                 placeholder = "Select data types")),
-             selectizeInput("firehoseExclude",
+             selectizeInput(id("firehoseExclude"),
                             "Files/archives to exclude", multiple = TRUE,
                             choices = c("RSEM_isoforms", ".aux.", ".mage-tab.",
                                         "MANIFEST.txt", "exon_quantification"),
@@ -80,13 +82,13 @@ addTCGAdata <- function() {
                             options = list(
                                 create = TRUE, createOnBlur=TRUE,
                                 placeholder = "Input files to exclude")),
-             textInput("dataFolder", "Folder to store the data",
+             textInput(id("dataFolder"), "Folder to store the data",
                        value = "~/Downloads",
                        placeholder = "Insert data folder"),
-             bsTooltip("dataFolder", placement = "right", 
+             bsTooltip(id("dataFolder"), placement = "right", 
                        options = list(container = "body"),
                        "Data not available in this folder will be downloaded."),
-             actionButton("getFirehoseData", "Get data"))
+             actionButton(id("getFirehoseData"), "Get data"))
     } else {
         list(p("Not able to reach Firehose."))
     }
@@ -98,7 +100,7 @@ ui <- function(tab) {
             sidebarPanel(
                 h3("Data input"),
                 shinyBS::bsCollapse(
-                    id = "addData",
+                    id = id("addData"),
                     open = "Add data",
                     shinyBS::bsCollapsePanel(
                         style = "info",
@@ -107,7 +109,7 @@ ui <- function(tab) {
                         addLocalFile())
                 ),
                 shinyBS::bsCollapse(
-                    id = "addTCGA",
+                    id = id("addTCGA"),
                     open = "Add TCGA/Firehose data",
                     shinyBS::bsCollapsePanel(
                         style = "info",
@@ -118,16 +120,16 @@ ui <- function(tab) {
             ),
             mainPanel(
                 # TODO(NunoA): Show alerts from renderUI
-                bsAlert(anchorId = "alert2"),
-                bsModal2("dataReplace", "Data already loaded", NULL,
+                bsAlert(anchorId = id("alert2")),
+                bsModal2(id("dataReplace"), "Data already loaded", NULL,
                          size = "small",
                          "Would you like to replace the loaded data?",
                          footer = list(
-                             actionButton("replace",
+                             actionButton(id("replace"),
                                           "data-dismiss"="modal", 
                                           label = "Replace"))),
-                uiOutput("tablesOrAbout"),
-                uiOutput("iframe"))))
+                uiOutput(id("tablesOrAbout")),
+                uiOutput(id("iframe")))))
 }
 
 #' Creates a tabPanel template for a datatable with a title and description
@@ -140,7 +142,7 @@ ui <- function(tab) {
 #' @return The HTML code for a tabPanel template
 #' @export
 tabTable <- function(title, id, description = NULL, ...) {
-    tablename <- paste("table", id, sep = "-")
+    tablename <- id(paste("table", id, sep = "-"))
     if(!is.null(description))
         d <- p(tags$strong("Table description:"), description,
                uiOutput(paste0(tablename, "-groups")),
@@ -155,23 +157,23 @@ tabTable <- function(title, id, description = NULL, ...) {
 #' 
 #' @return Part of the server logic related to this tab
 server <- function(input, output, session){
-    observeEvent(input$category, setCategory(input$category))
+    observeEvent(input[[id("category")]], setCategory(input[[id("category")]]))
     
     # Show welcome screen when there's no data loaded
-    output$tablesOrAbout <- renderUI({
+    output[[id("tablesOrAbout")]] <- renderUI({
         if(is.null(getData()))
             includeMarkdown("about.md")
         else
-            list(selectInput("category", "Select category:",
+            list(selectInput(id("category"), "Select category:",
                              choices = names(getData())),
-                 uiOutput("datatabs"))
+                 uiOutput(id("datatabs")))
     })
     
     # User files
-    observeEvent(input$acceptFile, {
+    observeEvent(input[[id("acceptFile")]], {
         error <- function(msg) { print(msg); return(NULL) }
-        if(is.null(input$dataFile)) error("No data input selected")
-        if(input$species == "") error("Species field can't be empty")
+        if(is.null(input[[id("dataFile")]])) error("No data input selected")
+        if(input[[id("species")]] == "") error("Species field can't be empty")
         
         # inFile <- input$dataFile
         # info <- read.table(inFile$datapath, sep = input$sep,
@@ -179,11 +181,11 @@ server <- function(input, output, session){
     })
     
     # The button is only enabled if it meets the conditions that follow
-    observe(toggleState("acceptFile", input$species != ""))
+    observe(toggleState(id("acceptFile"), input[[id("species")]] != ""))
     
     # Load Firehose data
     loadAllData <- reactive({
-        shinyjs::disable("getFirehoseData")
+        shinyjs::disable(id("getFirehoseData"))
         
         # # Direct download by the browser
         # source = paste0("https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/HT1425/",
@@ -196,19 +198,19 @@ server <- function(input, output, session){
         # Load data from Firehose
         setData(
             loadFirehoseData(
-                folder = input$dataFolder,
-                cohort = input$firehoseCohort,
-                date = gsub("-", "_", input$firehoseDate),
-                data_type = input$dataType,
-                exclude = input$firehoseExclude,
+                folder = input[[id("dataFolder")]],
+                cohort = input[[id("firehoseCohort")]],
+                date = gsub("-", "_", input[[id("firehoseDate")]]),
+                data_type = input[[id("dataType")]],
+                exclude = input[[id("firehoseExclude")]],
                 progress = updateProgress))
         
         closeProgress()
-        shinyjs::enable("getFirehoseData")
+        shinyjs::enable(id("getFirehoseData"))
     }) # end of reactive
     
     # Load data when the user presses to replace data
-    observeEvent(input$replace, loadAllData())
+    observeEvent(input[[id("replace")]], loadAllData())
     
     # Render tables when data changes
     observe({
@@ -220,15 +222,15 @@ server <- function(input, output, session){
     })
     
     # Check if data is already loaded and ask the user if it should be replaced
-    observeEvent(input$getFirehoseData, {
+    observeEvent(input[[id("getFirehoseData")]], {
         if (!is.null(getData()))
-            toggleModal(session, "dataReplace", "open")
+            toggleModal(session, id("dataReplace"), "open")
         else
             loadAllData()
     })
     
     # Render tabs with data tables
-    output$datatabs <- renderUI({
+    output[[id("datatabs")]] <- renderUI({
         categoryData <- getCategoryData()
         category <- getCategory()
         do.call(
@@ -237,21 +239,23 @@ server <- function(input, output, session){
                    function(i) {
                        tabTable(names(categoryData)[i],
                                 id = paste(category, i, sep = "-"),
-                                description = attr(categoryData[[i]], "description"))
+                                description = attr(categoryData[[i]],
+                                                   "description"))
                    })
         )
     }) # end of renderUI
     
     # Render a specific data table
     renderData <- function(index, data, group) {
-        tablename <- paste("table", getCategories()[group], index, sep = "-")
+        tablename <- id(paste("table", getCategories()[group], 
+                              index, sep = "-"))
         
         table <- data[[index]]
         if (isTRUE(attr(table, "rowNames")))
             table <- cbind(Row = rownames(table), table)
         
-        output[[paste0(tablename, "-groups")]] <- renderUI(
-            groupsUI(table, tablename))
+        # output[[paste0(tablename, "-groups")]] <- renderUI(
+            # groupsUI(table, tablename))
         
         # Subset to show default columns if any
         if (!is.null(attr(table, "show")))
@@ -274,20 +278,22 @@ server <- function(input, output, session){
             setGroups(groups[-1, ])
         }
         
-        if (input$subsetBy == "Column") {
+        subsetInput <- input[[id("subsetBy")]]
+        groupColumnInput <- input[[id("groupColumn")]]
+        if (subsetInput == "Column") {
             categoryData <- getCategoryData()
-            names <- unique(categoryData[[input$groupColumn]])
-            groups <- cbind(names, input$subsetBy, input$groupColumn)
+            names <- unique(categoryData[[groupColumnInput]])
+            groups <- cbind(names, subsetInput, groupColumnInput)
         } else {
-            elem <- switch(input$subsetBy,
+            elem <- switch(subsetInput,
                            "Rows" = input$groupRows,
                            "Expression" = input$groupExpression)
-            row <- c(input$groupName, input$subsetBy, elem)
+            row <- c(groupColumnInput, subsetInput, elem)
             groups <- rbind(getGroups(), row)
             rownames(groups) <- NULL
         }
         setGroups(groups)
     })
     
-    output$groupsTable <- renderTable(getGroups())
+    output[[id("groupsTable")]] <- renderTable(getGroups())
 }
