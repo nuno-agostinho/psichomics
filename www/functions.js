70312377b52001ba7218280403a0a455f9d60ae2
Nuno Agostinho <nunodanielagostinho@gmail.com>
2016-03-24 14:17:57 +0000

    Moved the JavaScript functions to a dedicated file and tried to add a check/uncheck all checkbox

---
+++ www/functions.js
@@ -0,0 +1,21 @@
+/* Check all groups when clicking the checkbox in the group table's header*/
+/* TODO(NunoA): Make this work... */
+$("input[name='checkAllGroups']").change(function () {
+    $("input[name='checkGroups']").prop('checked', $(this).prop("checked"));
+});
+
+/* Get which checkboxes are checked in the groups section */
+Shiny.addCustomMessageHandler('getCheckedBoxes', function(variable) {   
+    var selected = [];
+    $("input[name='checkGroups']:checked").each(function() {
+        selected.push($(this).attr('number'));
+        
+    });
+    /* Add value to variable in R */
+    Shiny.onInputChange(variable, selected);
+});
+
+/* Set given variable to zero */
+Shiny.addCustomMessageHandler('setZero', function(variable) {
+    Shiny.onInputChange(variable, 0);
+});
