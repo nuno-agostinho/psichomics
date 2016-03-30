0b1ca85eada03c6b24683fec583a3ef099b6cfcf
Nuno Agostinho <nunodanielagostinho@gmail.com>
2016-03-30 13:38:08 +0100

    Created two PCA modules to test ggplot2 and highcharts side-by-side

--- R/plots/pca_highcharts.R
+++ R/plots/pca_highcharts.R
@@ -1,6 +1,6 @@
 # The name used for the plot must be unique
-name <- "PCA ggplot2"
-id <- function(id) paste(name, id, sep = "_")
+plot <- "PCA highcharts"
+id <- function(value) objectId(name, plot, value)
 
 ui <- list(
     sidebarPanel(
@@ -11,40 +11,9 @@ ui <- list(
         actionButton(id("calculate"), "Calculate PCA"),
         uiOutput(id("selectPCA"))
     ), mainPanel(
-        plotOutput("scatterplot", 
-                   hover = hoverOpts("plotHover", delay = 100,
-                                     delayType = "throttle")),
-        uiOutput("hoverInfo")
+        highchartOutput(id("scatterplot"))
     )
 )
-
-#' Calculate mouse position according to mouse event
-#' 
-#' @references 
-#' Many thanks to Pawel Papkala for this bit of code. Adapted from
-#' http://www.77dev.com/2016/03/custom-interactive-csshtml-tooltips.html
-getMousePosition <- function(event) {
-    # Calculate point position INSIDE the image as percent of total dimensions
-    # from left (horizontal) and from top (vertical)
-    left_pct <- (event$x - event$domain$left) / (event$domain$right - event$domain$left)
-    top_pct <- (event$domain$top - event$y) / (event$domain$top - event$domain$bottom)
-    
-    # Calculate distance from left and bottom side of the picture in pixels
-    left_px <- event$range$left + left_pct * (event$range$right - event$range$left)
-    top_px <- event$range$top + top_pct * (event$range$bottom - event$range$top)
-    return(list("left"=left_px, "top"=top_px))
-}
-
-#' Create tooltip when interacting with a plot
-createTooltip <- function(event, ..., xDeviation = 0, yDeviation = 0) {
-    pos <- getMousePosition(event)
-    # Style of the tooltip
-    style <- paste0("position:absolute; z-index:100; color:white;",
-                    "background-color: rgba(0, 0, 0, .7);",
-                    "left:", pos$left + xDeviation,
-                    "px; top:", pos$top + yDeviation, "px;")
-    return(wellPanel(class = "well-sm", style = style, ...))
-}
 
 server <- function(input, output, session) {
     ir.pca <- reactiveValues()
@@ -72,7 +41,7 @@ server <- function(input, output, session) {
         )
     })
     
-    output$scatterplot <- renderPlot({
+    output[[id("scatterplot")]] <- renderHighchart({
         if (is.null(ir.pca$data)) 
             return(NULL)
         
@@ -86,28 +55,13 @@ server <- function(input, output, session) {
             label <- sprintf("%s (%s%% explained variance)", 
                              names(perc[c(xAxis, yAxis)]), 
                              round(perc[c(xAxis, yAxis)], 2))
-            
-            ggplot(data.frame(ir.pca$data$x), aes_string(x=xAxis, y=yAxis)) + 
-                geom_point() + labs(x=label[1], y=label[2])
+
+            df <- data.frame(ir.pca$data$x)
+            highchart() %>%
+                hc_add_series_scatter(df[[xAxis]], df[[yAxis]]) %>%
+                hc_xAxis(title = list(text = label[1])) %>%
+                hc_yAxis(title = list(text = label[2]))
         }
-    })
-    
-    output$hoverInfo <- renderUI({
-        if (is.null(ir.pca$data)) return(NULL)
-        
-        xAxis <- input[[id("pcX")]]
-        yAxis <- input[[id("pcY")]]
-        
-        # Get nearest point to hover event within 5 pixels
-        hover <- input$plotHover
-        point <- nearPoints(data.frame(ir.pca$data$x),
-                            hover, threshold = 5, maxpoints = 1, addDist = TRUE)
-        if (nrow(point) == 0) return(NULL)
-        
-        # Create tooltip from hover information
-        createTooltip(hover, xDeviation = 19, yDeviation = 2,
-                      tags$b(xAxis), point[[xAxis]], br(),
-                      tags$b(yAxis), point[[yAxis]])
     })
     
     output[[id("variancePlot")]] <- renderHighchart({
@@ -132,7 +86,7 @@ server <- function(input, output, session) {
             hc_exporting(enabled = TRUE,
                          buttons = list(
                              contextButton = list(text = "Export",
-                                                  verticalAlign = "bottom", 
+                                                  verticalAlign = "bottom",
                                                   y = -25)))
     })
 }
