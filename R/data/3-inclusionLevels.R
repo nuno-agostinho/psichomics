4a001d3b91f543eb2d4f23426d4ea8dce842171f
Nuno Agostinho <nunodanielagostinho@gmail.com>
2016-04-07 16:37:30 +0100

    Moved inclusion levels calculation to Data section and simplified the code

--- R/data/3-inclusionLevels.R
+++ R/data/3-inclusionLevels.R
@@ -1,46 +1,43 @@
-# The name used for the plot must be unique
-plot <- "Inclusion levels"
-id <- function(value) objectId(name, plot, value)
+name <- "Exon/intron inclusion"
 
-ui <- list(
-    sidebarPanel(
+# createLink <- function(val) {
+#     id <- gsub(" ", "_", val)
+#     js <- 'document.getElementById(\'%s\').selectize.setValue(\'%s\')'
+#
+#     onclick <- paste(sprintf(js, objectId(name, "selectizePlot"), "plot4"),
+#                      sprintf(js, objectId(name, "selectizeEvent"), id),
+#                      sep = "; ")
+#
+#     html <- paste('<a id="%s" title="Get more information about this event"',
+#                   'href="#" onclick="%s">%s</a>')
+#     link <- sprintf(html, id, onclick, val)
+#     return(link)
+# }
+
+ui <- function() {
+    list(
         selectizeInput(id("eventType"), "Event type",
                        choices = c("Skipping exon" = "SE"), selected = "SE",
                        multiple = TRUE),
         actionButton(id("calcIncLevels"), class = "btn-primary",
-                     "Calculate inclusion levels")),
-    mainPanel( dataTableOutput(id("incLevels")) )
-)
-
-createLink <- function(val) {
-    id <- gsub(" ", "_", val)
-    js <- 'document.getElementById(\'%s\').selectize.setValue(\'%s\')'
-    
-    onclick <- paste(sprintf(js, objectId(name, "selectizePlot"), "plot4"),
-                     sprintf(js, objectId(name, "selectizeEvent"), id),
-                     sep = "; ")
-    
-    html <- paste('<a id="%s" title="Get more information about this event"',
-                  'href="#" onclick="%s">%s</a>')
-    link <- sprintf(html, id, onclick, val)
-    return(link)
+                     "Calculate inclusion levels"))
 }
 
 server <- function(input, output, session) {
     levels <- reactive({
         eventType <- input[[id("eventType")]]
-        
+
         # Read annotation
         startProgress("Reading alternative splicing annotation",
                       divisions = 3)
         annot <- readAnnotation(eventType)
-        
+
         # Calculate inclusion levels with annotation and junction quantification
         updateProgress("Calculating inclusion levels")
         junctionQuant <- getJunctionQuantification()
         psi <- calculateInclusionLevels(eventType, junctionQuant, annot)
         setInclusionLevels(psi)
-        
+
         updateProgress("Matching clinical data")
         match <- matchIdWithClinical(colnames(psi), getClinicalData())
         # TODO: make sure this attribute allows to show row names in Data section
@@ -48,17 +45,11 @@ server <- function(input, output, session) {
         setClinicalMatchFrom("Inclusion levels", match)
         closeProgress()
     })
-    
+
     observeEvent(input[[id("calcIncLevels")]], {
         if(is.null(getData()) || is.null(getJunctionQuantification()))
             print("No junction quantification data!")
         else
             levels()
     })
-    
-    output[[id("incLevels")]] <- renderDataTable({
-        psi <- getInclusionLevels()
-        if (is.null(psi)) return(NULL)
-        cbind(Events = createLink(rownames(psi)), psi)
-    }, escape = FALSE, options = list(pageLength = 10, scrollX = TRUE))
 }
