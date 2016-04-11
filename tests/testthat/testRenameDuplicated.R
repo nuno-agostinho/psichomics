6a13d7f47215ead65f69cdcdb6d66cdfdd23aea4
Nuno Agostinho <nunodanielagostinho@gmail.com>
2016-04-11 13:43:33 +0100

    Characters in regex were not being escaped; renameDuplicated now only returns the vector with the values of interest

--- tests/testthat/testRenameDuplicated.R
+++ tests/testthat/testRenameDuplicated.R
@@ -3,33 +3,34 @@ context("Rename duplicated values")
 test_that("Return unique values in case there are duplicates", {
     check <- c("cat", "mouse", "snake")
     comp <- c("snake", "spider", "dog")
-    rep <- renameDuplicated(check, comp)
-    expect_equal(rep, c(comp, check[1:2], "snake (1)"))
+    res <- renameDuplicated(check, comp)
+    expect_equal(res, c(check[1:2], "snake (1)"))
 })
 
 test_that("Return unique values and count + 1 if needed", {
     check <- c("cat", "mouse", "snake")
     comp <- c("snake", "spider", "dog", "snake (2)", "snake (3)")
-    rep <- renameDuplicated(check, comp)
-    expect_equal(rep, c(comp, check[1:2], "snake (4)"))
+    res <- renameDuplicated(check, comp)
+    expect_equal(res, c(check[1:2], "snake (4)"))
 })
 
 test_that("Return same values in case no values are duplicated", {
     check <- c("cat", "mouse")
     comp <- c("snake", "spider", "dog")
-    rep <- renameDuplicated(check, comp)
-    expect_equal(rep, c(comp, check))
+    res <- renameDuplicated(check, comp)
+    expect_equal(res, c(check))
 })
 
-test_that("Return comp values only if argument check is of length 0", {
+test_that("Return no check values if argument check has length 0", {
     check <- character(0)
     comp <- c("cat", "mouse")
-    rep <- renameDuplicated(check, comp)
-    expect_equal(rep, comp)
+    res <- renameDuplicated(check, comp)
+    expect_equal(res, check)
 })
 
-test_that("Return check values only if argument comp is of length 0", {
+test_that("Return the same check values if argument comp is of length 0", {
     check <- c("cat", "mouse")
     comp <- character(0)
-    expect_error(renameDuplicated(check, comp))
+    res <- renameDuplicated(check, comp)
+    expect_equal(res, check)
 })
