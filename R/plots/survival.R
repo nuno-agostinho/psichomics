20aab9ec4f0eb909481cb7cb27eeeb36be251009
Nuno Agostinho <nunodanielagostinho@gmail.com>
2016-04-27 18:17:33 +0100

    First take into survival analysis plot using Kaplan-Meier estimator: choose time start/stop and censoring type in case of only start time is selected

--- R/plots/survival.R
+++ R/plots/survival.R
@@ -1,12 +1,113 @@
+## TODO(NunoA): How to select events?
+## TODO(NunoA): How to select groups? If there are intersections, merge them
+## (maybe warn the user? maybe not needed as he'll see it?)
+
 # The name used for the plot must be unique
 plot <- "Survival plots"
 id <- function(value) objectId(name, plot, value)
 
-ui <- plotOutput(id(plot))
+ui <- tagList(
+    sidebarPanel(
+        selectizeInput(id("timeStart"), choices = NULL,
+                       "Clinical attribute for starting time"),
+        selectizeInput(id("timeStop"), choices = NULL,
+                       "Clinical attribute for ending time (optional)"),
+        helpText("In case there's no record for a sample, the days to last",
+                 "follow up will be used instead."),
+        conditionalPanel(paste0("input.", id("timeStop"), "==''"),
+                         radioButtons(id("censoring"), "Data censoring",
+                                      selected="right",
+                                      choices = c(Left="left", Right="right"))),
+        actionButton(id("plot"), class="btn-primary",
+                     tagList(icon("medkit"), "Plot survival curves"))
+    ),
+    mainPanel(
+        plotOutput(id(plot))
+    )
+)
+
+#' Get the max number of days recorded of each sample for a given column
+#'
+#' @param col Character: column of interest
+#' @param clinical Data.frame: clinical data
+#'
+#' @return Numeric vector with days recorded for given column (with NA when data
+#' is not given)
+sampleDays <- function(col, clinical) {
+    cols <- grep(col, names(clinical))
+    row <- apply(clinical[cols], 1, function(i)
+        if(!all(is.na(i))) max(as.numeric(i), na.rm = TRUE) else NA)
+    return(row)
+}
 
 server <- function(input, output, session) {
-    output[[id(plot)]] <- renderPlot({
-        # validate(if(is.null(thisData$data)) return(NULL))
-        ggplot(data=mtcars, aes(cyl, mpg)) + geom_bin2d()
+    # Update every time clinical data changes
+    observe({
+        clinical <- getClinicalData()
+        if (!is.null(clinical)) {
+            # Allow the user to select any "days_to" attribute available
+            daysTo <- grep("days_to_", names(clinical), value=TRUE, fixed=TRUE)
+            subDaysTo <- gsub(".*(days_to_.*)", "\\1", daysTo)
+            choices <- unique(subDaysTo)
+            names(choices) <- gsub("_", " ", choices, fixed=TRUE)
+            names(choices) <- R.utils::capitalize(names(choices))
+            updateSelectizeInput(session, id("timeStart"), choices = choices)
+            updateSelectizeInput(
+                session, id("timeStop"), choices = choices, options = list(
+                    placeholder = "Select a column to use interval data",
+                    onInitialize = I('function() { this.setValue(""); }')))
+        }
+    })
+    
+    # Plot survival curve
+    observeEvent(input[[id("plot")]], {
+        output[[id(plot)]] <- renderPlot({
+            isolate({
+                # Get clinical data and column of interest
+                clinical <- getClinicalData()
+                timeStart <- input[[id("timeStart")]]
+                timeStop <- input[[id("timeStop")]]
+                censoring <- input[[id("censoring")]]
+            })
+            if (is.null(clinical)) {
+                errorModal(session, "Clinical data missing",
+                           "Insert clinical data first.")
+            } else {
+                rownames(clinical) <- toupper(
+                    clinical$patient.bcr_patient_barcode)
+                
+                # Save the days from columns of interest in a data frame
+                if (timeStop == "") timeStop <- NULL
+                cols <- c("days_to_last_followup", timeStart, timeStop)
+                colsDays <- lapply(cols, sampleDays, clinical)
+                names(colsDays) <- cols
+                colsDays <- as.data.frame(colsDays)
+                
+                # Create new time using the starting time replacing the NAs with
+                # days to last follow up
+                nas <- is.na(colsDays[[2]])
+                colsDays$time <- colsDays[[2]]
+                colsDays$time[nas] <- colsDays[[1]][nas]
+                
+                # Indicate event of interest and groups
+                colsDays$event <- ifelse(!nas, 1, 0)
+                colsDays$groups <- rep("A", nrow(colsDays))
+                
+                # Estimate and plot survival curves by groups
+                if (is.null(timeStop))
+                    form <- Surv(time, event, type = censoring) ~ groups
+                else {
+                    # Create new time using the ending time replacing the NAs
+                    # with days to last follow up
+                    nas <- is.na(colsDays[[3]])
+                    colsDays$time2 <- colsDays[[3]]
+                    colsDays$time2[nas] <- colsDays[[1]][nas]
+                    
+                    form <- Surv(time, time2, event, type = "interval") ~ groups
+                }
+                surv <- survfit(form, data = colsDays)
+                plot(surv, lty = 2:3)
+            }
+        })
     })
 }
