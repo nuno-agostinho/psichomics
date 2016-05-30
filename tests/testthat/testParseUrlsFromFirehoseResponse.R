ceae13801ee60017c129299e1e32c359b065ba68
Nuno Agostinho <nunodanielagostinho@gmail.com>
2016-05-30 21:25:26 +0100

    Update test to reflect changes on function

--- tests/testthat/testParseUrlsFromFirehoseResponse.R
+++ tests/testthat/testParseUrlsFromFirehoseResponse.R
@@ -4,5 +4,5 @@ test_that("Parse the URLs from a Firehose response", {
     res <- queryFirehoseData(cohort = "ACC")
     url <- parseUrlsFromFirehoseResponse(res)
     expect_equal(length(unique(names(url))), 1)
-    expect_true(grepl("ACC", unique(names(url))))
+    expect_true(grepl("Adrenocortical carcinoma", unique(names(url))))
 })
