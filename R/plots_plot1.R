b9a77052dc2acd8b7a95c64b277751c62faa28c1
Nuno Agostinho <nunodanielagostinho@gmail.com>
2016-06-09 00:12:32 +0100

    Make plots work in package

--- R/plots_plot1.R
+++ R/plots_plot1.R
@@ -1,27 +1,28 @@
-# The name used for the plot must be unique
-plot <- "plot1"
-id <- function(value) objectId(name, plot, value)
-
-ui <- function() {
-    list(
+plots1UI <- function(id) {
+    ns <- NS(id)
+    tagList(
         sidebarLayout(
             sidebarPanel(
-                selectizeInput(id("x"), "Pick x axis", choices = names(mtcars)),
-                selectizeInput(id("y"), "Pick y axis", choices = names(mtcars)),
-                actionButton(id("change"), "Change to plot2")
+                selectizeInput(ns("x"), "Pick x axis", choices = names(mtcars)),
+                selectizeInput(ns("y"), "Pick y axis", choices = names(mtcars))
+                # actionButton(ns("change"), "Change to plot2")
             ), 
-            mainPanel( plotOutput(id(plot)) )
+            mainPanel( plotOutput(ns("plot")) )
         )
     )
 }
 
-server <- function(input, output, session) {
-    output[[id(plot)]] <- renderPlot({
-        ggplot(data=mtcars, aes_string(input[[id("x")]], input[[id("y")]])) +
-            geom_bin2d()
+plots1Server <- function(input, output, session) {
+    output$plot <- renderPlot({
+        plot(mtcars[[input$x]], mtcars[[input$y]])
+        # ggplot(data=mtcars, aes_string(input$x, input$y)) + geom_bin2d()
     })
     
-    observeEvent(input[[id("change")]], {
-        updateSelectizeInput(session, "Plots_selectizePlot", selected = "plot2")
-    })
+    # observeEvent(input$change, {
+    #     updateSelectizeInput(session, "Plots_selectizePlot", selected="plot2")
+    # })
 }
+
+attr(plots1UI, "loader") <- "plots"
+attr(plots1UI, "name") <- "Plots 1"
+attr(plots1Server, "loader") <- "plots"
