8af9e1bdd1da6d18965e2159c9040b09faf252d1
Nuno Agostinho <nunodanielagostinho@gmail.com>
2016-06-08 01:33:35 +0100

    Fix widgets to run as a package

--- R/widgets.R
+++ R/widgets.R
@@ -1,46 +1,43 @@
-name <- "Widgets"
-primary <- TRUE
-id <- function(value) objectId(name, value)
-
 #' User interface
 #' @importFrom shiny column
 #' @importFrom shinyBS bsAlert
-ui <- function(tab) {
-    tab(name,
+widgetsUI <- function(id, tab) {
+    ns <- NS(id)
+    tab("Widgets",
         fluidRow(
-            bsAlert(anchorId = id("alert")),
+            uiOutput(ns("localModal")),
             column(3, h3("Buttons"),
-                          actionButton(id("action"), label = "Action")),
+                          actionButton(ns("action"), label = "Action")),
             #submitButton("Submit")), # DOESN'T ALLOW TO UPDATE VALUES...
             column(3, h3("Single checkbox"),
-                          checkboxInput(id("checkbox"), label = "Choice A",
+                          checkboxInput(ns("checkbox"), label = "Choice A",
                                         value = TRUE)),
             column(3, checkboxGroupInput(
-                id("checkGroup"), label = h3("Checkbox group"), 
+                ns("checkGroup"), label = h3("Checkbox group"), 
                 choices = list("Choice 1" = 1, "Choice 2" = 2,
                                "Choice 3" = 3), selected = 1)),
-            column(3, dateInput(id("date"),
+            column(3, dateInput(ns("date"),
                                        label = h3("Date input"),
                                        value = "2014-01-01"))   
         ),
         fluidRow(
-            column(3, dateRangeInput(id("dates"), label = h3("Date range"))),
-            column(3, fileInput(id("file"), label = h3("File input"))),
+            column(3, dateRangeInput(ns("dates"), label = h3("Date range"))),
+            column(3, fileInput(ns("file"), label = h3("File input"))),
             column(3, h3("Help text"),
                           helpText("Note: help text isn't a true widget,", 
                                    "but it provides an easy way to add text to",
                                    "accompany other widgets.")),
-            column(3, numericInput(id("num"), label = h3("Numeric input"),
+            column(3, numericInput(ns("num"), label = h3("Numeric input"),
                                           value = 1))   
         ),
         fluidRow(
             column(3, radioButtons(
-                id("radio"), label = h3("Radio buttons"),
+                ns("radio"), label = h3("Radio buttons"),
                 choices = list("Choice 1" = 1,
                                "Choice 2" = 2,
                                "Choice 3" = 3), selected = 1)),
             column(3, selectizeInput(
-                id("select"), label = h3("Selectize box"),
+                ns("select"), label = h3("Selectize box"),
                 choices = list("Primates" = c("Homo sapiens (human)" = 1),
                                "Rodents" = c("Mus musculus (mouse)" = 2),
                                "Carnivora" = c("Canis lupus (wolf)" = 3)),
@@ -49,31 +46,38 @@ ui <- function(tab) {
                                addPrecedence = TRUE),
                 selected = 1, multiple = TRUE, size = 2)),
             column(3,
-                          sliderInput(id("slider1"), label = h3("Sliders"), min = 0,
+                          sliderInput(ns("slider1"), label = h3("Sliders"), min = 0,
                                       max = 100, value = 50),
-                          sliderInput(id("slider2"), "", min = 0, max = 100,
+                          sliderInput(ns("slider2"), "", min = 0, max = 100,
                                       value = c(25, 75))),
-            column(3, textInput(id("text"), label = h3("Text input"), 
+            column(3, textInput(ns("text"), label = h3("Text input"), 
                                        value = "Enter text..."))   
         )
     )
 }
 
-#' @importFrom shinyBS createAlert closeAlert
-server <- function(input, output, session) {
+#' @importFrom shinyBS bsModal
+widgetsServer <- function(input, output, session) {
+    ns <- session$ns
+    
     obsB <- observe({
         # Dismiss the alert or it won't update afterwards
-        closeAlert(session, alertId = id("exampleAlert"))
-        if (input[[id("slider1")]] > 90)
-            createAlert(session, anchorId = id("alert"),
-                        alertId = id("exampleAlert"),
-                        title = "Awesome", content = "Over 90! Fantastic.",
-                        style = "success")
-        else if (input[[id("slider1")]] > 75)
-            createAlert(session, id("alert"), id("exampleAlert"), title = "Great",
-                        content = "Above 75!")
-        else if (input[[id("slider1")]] < 25)
-            createAlert(session, id("alert"), id("exampleAlert"), title = "Too low",
-                        content = "Turn it up a bit, please.", style = "danger")
+        if (input$slider1 > 90) {
+            output$localModal <- renderUI(
+                bsModal(ns("modalExample"), "Awesome!", NULL, "Over 90!")
+            )
+        } else if (input$slider1 > 75) {
+            output$localModal <- renderUI(
+                bsModal(ns("modalExample"), "Great", NULL, "It's something.")
+            )
+        } else if (input$slider1 < 25) {
+            output$localModal <- renderUI(
+                bsModal(ns("modalExample"), "Too low", NULL, "Turn it up a bit")
+            )
+        } else return(NULL)
+        toggleModal(session, "modalExample", toggle="open")
     })
 }
+
+attr(widgetsUI, "loader") <- "app"
+attr(widgetsServer, "loader") <- "app"
