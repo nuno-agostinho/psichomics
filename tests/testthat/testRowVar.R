9899f880d552ea7306c49a21b679180392de1142
Nuno Agostinho <nunodanielagostinho@gmail.com>
2016-06-29 16:13:15 +0100

    Fix Travis-CI error: "Objects equal but not identical"

--- tests/testthat/testRowVar.R
+++ tests/testthat/testRowVar.R
@@ -4,7 +4,7 @@ test_that("Calculate variance for a one-row matrix", {
     mat <- matrix(rnorm(500), nrow = 1)
     res1 <- rowVar(mat)
     res2 <- apply(mat, 1, var) # R way
-    expect_identical(signif(res1, 12), signif(res2, 12))
+    expect_equal(signif(res1, 12), signif(res2, 12))
 })
 
 test_that("Calculate variance for a one-row matrix ignoring NAs", {
@@ -14,19 +14,19 @@ test_that("Calculate variance for a one-row matrix ignoring NAs", {
     # Ignore NAs
     res1 <- rowVar(mat, na.rm = TRUE)
     res2 <- apply(mat, 1, var, na.rm = TRUE) # R way
-    expect_identical(signif(res1, 12), signif(res2, 12))
+    expect_equal(signif(res1, 12), signif(res2, 12))
     
     # Don't ignore NAs
     res1 <- rowVar(mat, na.rm = FALSE)
     res2 <- apply(mat, 1, var, na.rm = FALSE) # R way
-    expect_identical(signif(res1, 12), signif(res2, 12))
+    expect_equal(signif(res1, 12), signif(res2, 12))
 })
 
 test_that("Calculate variance for a multi-row matrix", {
     mat <- replicate(500, rnorm(500))
     res1 <- rowVar(mat)
     res2 <- apply(mat, 1, var) # R way
-    expect_identical(signif(res1, 12), signif(res2, 12))
+    expect_equal(signif(res1, 12), signif(res2, 12))
 })
 
 test_that("Calculate variance for a multi-row matrix", {
@@ -36,10 +36,10 @@ test_that("Calculate variance for a multi-row matrix", {
     # Ignore NAs
     res1 <- rowVar(mat, na.rm = TRUE)
     res2 <- apply(mat, 1, var, na.rm = TRUE) # R way
-    expect_identical(signif(res1, 12), signif(res2, 12))
+    expect_equal(signif(res1, 12), signif(res2, 12))
     
     # Don't ignore NAs
     res1 <- rowVar(mat, na.rm = FALSE)
     res2 <- apply(mat, 1, var, na.rm = FALSE) # R way
-    expect_identical(signif(res1, 12), signif(res2, 12))
+    expect_equal(signif(res1, 12), signif(res2, 12))
 })
