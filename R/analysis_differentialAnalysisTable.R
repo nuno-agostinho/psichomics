634ee9b65d31fc790b7556cfd1e5121b0974494e
Nuno Agostinho <nunodanielagostinho@gmail.com>
2016-07-15 15:43:30 +0100

    Calculate optimal survival cut-offs

--- R/analysis_differentialAnalysisTable.R
+++ R/analysis_differentialAnalysisTable.R
@@ -15,11 +15,10 @@ diffAnalysisTableUI <- function(id) {
                       "Levene's test (2 or more groups)"="levene"),
                     selected=c("basicStats", "wilcox", "kruskal", "levene")),
                 # Disable checkbox of basic statistics
-                HTML("<script>",
-                     '$("[value=basicStats]").attr("disabled", true);', 
-                     "</script>"),
+                tags$script('$("[value=basicStats]").attr("disabled", true);'),
                 actionButton(ns("startAnalyses"), class="btn-primary", 
-                             "Perform analyses")
+                             "Perform analyses"),
+                uiOutput(ns("survivalOptions"))
             ), mainPanel(
                 uiOutput(ns("showColumns")),
                 dataTableOutput(ns("statsTable")),
@@ -249,6 +248,161 @@ diffAnalysisTableServer <- function(input, output, session) {
                 write.table(getDifferentialAnalyses()[-c("Density")], 
                             file, quote=FALSE, row.names=FALSE, sep="\t")
         )
+    })
+    
+    observe({
+        if (is.null(getDifferentialAnalyses()) || is.null(getClinicalData()))
+            return(NULL)
+        
+        output$survivalOptions <- renderUI({
+            tagList(
+                hr(),
+                h3("Survival analyses"),
+                radioButtons(ns("censoring"), "Data censoring",
+                             selected="right",
+                             inline=TRUE, choices=c(
+                                 Left="left",
+                                 Right="right",
+                                 Interval="interval",
+                                 "Interval 2" = "interval2")),
+                selectizeInput(ns("timeStart"), choices = NULL, 
+                               "Follow up time"),
+                # If the chosen censoring contains the word 'interval',
+                # show this input
+                conditionalPanel(
+                    paste0("input[id='", ns("censoring"),
+                           "'].indexOf('interval') > -1"),
+                    selectizeInput(ns("timeStop"), choices=NULL, 
+                                   "Ending time")),
+                helpText("In case there's no record for a patient, the",
+                         "days to last follow up will be used instead."),
+                selectizeInput(ns("event"), choices = NULL, 
+                               "Event of interest"),
+                radioButtons(
+                    ns("selected"), "Perform survival analysis in:",
+                    choices=c("Alternative splicing events currently in the table"="shown",
+                              "All alternative splicing events (slow process)"="all")),
+                actionButton(ns("survival"), class="btn-primary",
+                             "Perform survival analysis")
+            )
+        })
+        
+        # Update selectize input label depending on the chosen censoring type
+        observe({
+            if (is.null(input$censoring)) return(NULL)
+            
+            label <- "Follow up time"
+            if (grepl("interval", input$censoring, fixed=TRUE))
+                label <- "Starting time"
+            updateSelectizeInput(session, "timeStart", label=label)
+        })
+        
+        updateClinicalFields(session)
+    })
+    
+    observeEvent(input$survival, {
+        survTerms <- isolate({
+            # Get tumour sample IDs (normal and control samples are not
+            # interesting for survival analysis)
+            match <- getClinicalMatchFrom("Inclusion levels")
+            types <- getSampleTypes(names(match))
+            tumour <- match[!grepl("Normal|Control", types)]
+            
+            # Group samples by the inclusion levels cut-off
+            clinical <- getClinicalData()
+            clinicalIDs <- nrow(clinical)
+            groups <- rep(NA, clinicalIDs)
+            
+            censoring <- input$censoring
+            timeStart <- input$timeStart
+            timeStop  <- input$timeStop
+            dataEvent <- input$event
+            psi       <- getInclusionLevels()
+            stats     <- getDifferentialAnalyses()
+            display   <- input$statsTable_rows_current
+            selected  <- input$selected
+        })
+        
+        if (input$selected == "shown")
+            psi <- psi[display, ]
+        
+        startProgress("Performing survival analysis", nrow(psi))
+        
+        opt <- apply(psi, 1, function(vector) {
+            v <- as.numeric(vector[toupper(names(tumour))])
+            
+            #' @importFrom survival survdiff
+            testSurvival2 <- function(psiCutoff, groups, tumour, psi, clinical, 
+                                      censoring, timeStart, timeStop, dataEvent,
+                                      survTime) {
+                groups[tumour] <- psi >= psiCutoff
+                
+                # Assign a value based on the inclusion levels cut-off
+                # groups[is.na(groups)] <- "NA"
+                groups[groups == "TRUE"]  <- paste("Inclusion levels >=", psiCutoff)
+                groups[groups == "FALSE"] <- paste("Inclusion levels <", psiCutoff)
+                fillGroups <- groups
+                
+                processSurvTerms2 <- function(group, clinical, censoring, timeStart, 
+                                              timeStop, dataEvent, survTime) {
+                    # Ignore timeStop if interval-censoring is not selected
+                    if (!grepl("interval", censoring, fixed=TRUE) || 
+                        timeStop == "") 
+                        timeStop <- NULL
+                    
+                    # Check if using or not interval-censored data
+                    formulaSurv <- ifelse(is.null(timeStop),
+                                          "Surv(time, event, type=censoring) ~", 
+                                          "Surv(time, time2, event, type=censoring) ~")
+                    
+                    survTime <- processSurvData(timeStart, timeStop, dataEvent, 
+                                                group, clinical)
+                    
+                    form <- tryCatch(formula(paste(formulaSurv, "groups")), 
+                                     error = return)
+                    if ("simpleError" %in% class(form)) return(NULL)
+                    res <- list(form=form, survTime=survTime)
+                    return(res)
+                }
+                
+                # Calculate survival curves
+                survTerms <- processSurvTerms2(fillGroups, clinical, censoring, 
+                                               timeStart, timeStop, dataEvent,
+                                               survTime)
+                form <- survTerms$form
+                data <- survTerms$survTime
+                
+                # If there's an error with survdiff, return NA
+                pvalue <- tryCatch({
+                    # Test the difference between survival curves
+                    diff <- survdiff(form, data = data)
+                    
+                    # Calculate p-value with 5 significant numbers
+                    pvalue <- 1 - pchisq(diff$chisq, length(diff$n) - 1)
+                    return(as.numeric(signifDigits(pvalue)))
+                }, error = function(e) NA)
+                return(pvalue)
+            }
+            
+            opt <- suppressWarnings(
+                optim(0, testSurvival2, groups=groups, tumour=tumour, psi=v, 
+                      clinical=clinical, censoring=censoring, timeStart=timeStart,
+                      timeStop=timeStop, dataEvent=dataEvent, survTime=survTime,
+                      # Method and parameters interval
+                      method="Brent", lower=0, upper=1))
+            
+            updateProgress("Survival analysis", console=FALSE)
+            return(c("Optimal survival PSI cut-off"=opt$par, 
+                     "Optimal survival difference"=opt$value))
+        })
+        # Remove NAs and add information to the statistical table
+        opt <- opt[ , !is.na(opt[2, ])]
+        df <- data.frame(t(opt))
+        stats[names(df)] <- NA
+        stats[rownames(df), names(df)] <- df
+        
+        setDifferentialAnalyses(stats)
+        closeProgress()
     })
 }
 
