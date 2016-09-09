c18bf507570455235fe9db84a80b26c4722bbc6b
Nuno Agostinho <nunodanielagostinho@gmail.com>
2016-09-09 15:05:18 +0100

    Replace Levene's test function
    - Faster
    - Previous code was not MIT-license compliant

---
+++ tests/testthat/testLeveneTest.R
@@ -0,0 +1,56 @@
+context("Perform Levene's test")
+
+data <- car::Moore
+values <- data$conformity
+groups <- data$fcategory
+
+test_that("Calculate spread using the median values per group", {
+    lev <- leveneTest(values, groups)
+    car <- car::leveneTest(values, groups)
+    
+    expect_is(lev, "htest")
+    expect_equal(lev$statistic[[1]], car$`F value`[[1]])
+    expect_equal(lev$p.value, car$`Pr(>F)`[[1]])
+    expect_true(grepl("median", lev$method))
+})
+
+test_that("Calculate spread using the mean values per group", {
+    lev <- leveneTest(values, groups, "mean")
+    car <- car::leveneTest(values, groups, "mean")
+    
+    expect_is(lev, "htest")
+    expect_equal(lev$statistic[[1]], car$`F value`[[1]])
+    expect_equal(lev$p.value, car$`Pr(>F)`[[1]])
+    expect_true(grepl("mean", lev$method))
+})
+
+
+test_that("Remove missing values", {
+    random <- round(runif(5, 1, length(values)))
+    values[random] <- NA
+    
+    lev <- leveneTest(values, groups)
+    car <- car::leveneTest(values, groups)
+    
+    expect_is(lev, "htest")
+    expect_equal(lev$statistic[[1]], car$`F value`[[1]])
+    expect_equal(lev$p.value, car$`Pr(>F)`[[1]])
+    expect_true(grepl("median", lev$method))
+})
+
+test_that("Factorise groups if groups are not factors", {
+    lev_factor <- leveneTest(values, groups)
+    expect_is(lev_factor, "htest")
+    
+    groups_char <- as.character(groups)
+    
+    lev_char <- leveneTest(values, groups_char)
+    expect_is(lev_char, "htest")
+    expect_equal(lev_factor$statistic, lev_char$statistic)
+    expect_equal(lev_factor$p.value, lev_char$p.value)
+    
+    car <- suppressWarnings( car::leveneTest(values, groups_char) )
+    expect_equal(lev_char$statistic[[1]], car$`F value`[[1]])
+    expect_equal(lev_char$p.value, car$`Pr(>F)`[[1]])
+    expect_true(grepl("median", lev_char$method))
+})
